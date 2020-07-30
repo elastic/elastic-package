83488ca1c11e1f2d049e404aa634192461c7e689
mtojek <marcin.tojek@elastic.co>
2020-07-30 11:49:59 +0200

    Disable test from check

--- cmd/check.go
+++ cmd/check.go
@@ -9,13 +9,12 @@ func setupCheckCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "check",
 		Short: "Check the integration",
-		Long:  "Use check command to verify if the integration is correct in terms of formatting, validation, building, tests.",
+		Long:  "Use check command to verify if the integration is correct in terms of formatting, validation and building.",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			err := composeCommandActions(cmd, args,
 				formatCommandAction,
 				lintCommandAction,
 				buildCommandAction,
-				testCommandAction,
 			)
 			if err != nil {
 				return errors.Wrap(err, "checking integration failed")
