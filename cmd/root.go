a4d52ef8b6d8b3389a475f4a2ccf6451c62abb5b
Shaunak Kashyap <ycombinator@gmail.com>
2020-08-11 04:35:32 -0700

    Implement lint subcommand (#42)
    * Implementing lint subcommand
    
    * Silence usage on validation errors
    
    * Re-fetching validator package
    
    * Sort imports
    
    * Re-formatting
    
    * Move SilenceUsage to root cmd
    
    * Fixing mistake made in rebase
    
    * Refactoring: extracting FindPackageRoot
    
    * Adding godocs

--- cmd/root.go
+++ cmd/root.go
@@ -7,8 +7,9 @@ import (
 // RootCmd creates and returns root cmd for elastic-package
 func RootCmd() *cobra.Command {
 	rootCmd := &cobra.Command{
-		Use:   "elastic-package",
-		Short: "elastic-package - Command line tool for developing Elastic packages",
+		Use:          "elastic-package",
+		Short:        "elastic-package - Command line tool for developing Elastic Integrations",
+		SilenceUsage: true,
 	}
 	rootCmd.AddCommand(
 		setupCheckCommand(),
