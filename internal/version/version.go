ec622631412af26470a7a203f3f0b1f7c9270e36
mtojek <marcin.tojek@elastic.co>
2020-08-20 09:04:41 +0200

    Fix: missing version in filename

--- internal/version/version.go
+++ internal/version/version.go
@@ -7,7 +7,7 @@ import (
 
 var (
 	// BuildTime is the build time of the binary (set externally with ldflags).
-	BuildTime string
+	BuildTime = "unknown"
 
 	// CommitHash is the Git hash of the branch, used for version purposes (set externally with ldflags).
 	CommitHash = "undefined"
@@ -15,8 +15,8 @@ var (
 
 // BuildTimeFormatted method returns the build time preserving the RFC3339 format.
 func BuildTimeFormatted() string {
-	if BuildTime == "" {
-		return "unknown"
+	if BuildTime == "unknown" {
+		return BuildTime
 	}
 
 	seconds, err := strconv.ParseInt(BuildTime, 10, 64)
