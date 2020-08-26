4529add555591b2e5db6ffd0a28aebc45159f5ec
Shaunak Kashyap <ycombinator@gmail.com>
2020-08-26 05:01:48 -0700

    Add elastic-agent serice to snapshot docker-compose file

--- internal/install/static_snapshot_yml.go
+++ internal/install/static_snapshot_yml.go
@@ -69,4 +69,23 @@ services:
     depends_on:
       package-registry:
         condition: service_healthy
+
+  elastic-agent:
+    image: docker.elastic.co/beats/elastic-agent:${STACK_VERSION}
+    depends_on:
+      elasticsearch:
+        condition: service_healthy
+      kibana:
+        condition: service_healthy
+    environment:
+    - "FLEET_ENROLL=1"
+    - "FLEET_ENROLL_INSECURE=1"
+    - "FLEET_SETUP=1"
+    - "KIBANA_HOST=http://kibana:5601"
+
+  elastic-agent_is_ready:
+    image: tianon/true
+    depends_on:
+      elastic-agent:
+        condition: service_healthy
 `
