e36b546a8c83400d108fb6e8fc1129d4e109e8d3
Shaunak Kashyap <ycombinator@gmail.com>
2020-09-15 14:25:39 -0400

    Expect service deployment related files in _dev/deploy (#98)

--- internal/testrunner/runners/system/servicedeployer/factory.go
+++ internal/testrunner/runners/system/servicedeployer/factory.go
@@ -7,7 +7,7 @@ package servicedeployer
 import (
 	"errors"
 	"os"
-	"path"
+	"path/filepath"
 )
 
 var (
@@ -19,10 +19,10 @@ var (
 // Factory chooses the appropriate service runner for the given package, depending
 // on service configuration files defined in the package.
 func Factory(packageRootPath string) (ServiceDeployer, error) {
-	packageDevPath := path.Join(packageRootPath, "_dev")
+	packageDevPath := filepath.Join(packageRootPath, "_dev")
 
 	// Is the service defined using a docker compose configuration file?
-	dockerComposeYMLPath := path.Join(packageDevPath, "docker-compose.yml")
+	dockerComposeYMLPath := filepath.Join(packageDevPath, "deploy", "docker-compose.yml")
 	if _, err := os.Stat(dockerComposeYMLPath); err == nil {
 		return NewDockerComposeServiceDeployer(dockerComposeYMLPath)
 	}
