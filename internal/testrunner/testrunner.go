f1945d168e33a2173c1d090bfc509294b566fbb1
Marcin Tojek <mtojek@users.noreply.github.com>
2020-09-30 13:52:04 +0200

    Update internal/testrunner/testrunner.go
    Co-authored-by: Shaunak Kashyap <ycombinator@gmail.com>

--- internal/testrunner/testrunner.go
+++ internal/testrunner/testrunner.go
@@ -30,7 +30,7 @@ type RunFunc func(options TestOptions) error
 
 var runners = map[TestType]RunFunc{}
 
-// TestFolder encapsulates the test folder path and names of the package + dataStream
+// TestFolder encapsulates the test folder path and names of the package + data stream
 // to which the test folder belongs.
 type TestFolder struct {
 	Path       string
