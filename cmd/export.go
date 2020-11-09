ff4aae284034c007cd2e923e47206e8d142f516e
mtojek <marcin.tojek@elastic.co>
2020-11-09 13:06:09 +0100

    make format

--- cmd/export.go
+++ cmd/export.go
@@ -4,10 +4,9 @@ import (
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 
-	"github.com/elastic/elastic-package/internal/kibana/dashboards"
-
 	"github.com/elastic/elastic-package/internal/cobraext"
 	"github.com/elastic/elastic-package/internal/export"
+	"github.com/elastic/elastic-package/internal/kibana/dashboards"
 )
 
 func setupExportCommand() *cobra.Command {
