c4a6a69e6a0576021b61b763f90382ae218c956a
Adrian Serrano <adrisr83@gmail.com>
2020-12-04 10:44:10 +0100

    go fmt

--- internal/fields/validate_test.go
+++ internal/fields/validate_test.go
@@ -50,11 +50,11 @@ func TestValidate_WithFlattenedFields(t *testing.T) {
 
 func Test_parseElementValue(t *testing.T) {
 	for _, test := range []struct {
-		key string
-		value interface{}
+		key        string
+		value      interface{}
 		definition FieldDefinition
-		fail bool
-	} {
+		fail       bool
+	}{
 		// Arrays (only first value checked)
 		{
 			key:   "string array to keyword",
@@ -74,43 +74,43 @@ func Test_parseElementValue(t *testing.T) {
 
 		// keyword and constant_keyword (string)
 		{
-			key: "constant_keyword with pattern",
+			key:   "constant_keyword with pattern",
 			value: "some value",
 			definition: FieldDefinition{
-				Type: "constant_keyword",
+				Type:    "constant_keyword",
 				Pattern: `^[a-z]+\s[a-z]+$`,
 			},
 		},
 		{
-			key: "constant_keyword fails pattern",
+			key:   "constant_keyword fails pattern",
 			value: "some value",
 			definition: FieldDefinition{
-				Type: "constant_keyword",
+				Type:    "constant_keyword",
 				Pattern: `[0-9]`,
 			},
 			fail: true,
 		},
 		// keyword and constant_keyword (numeric)
 		{
-			key: "numeric keyword works",
+			key:   "numeric keyword works",
 			value: 1234.5,
 			definition: FieldDefinition{
-				Type: "keyword",
+				Type:    "keyword",
 				Pattern: `^[0-9.]+$`,
 			},
 		},
 		{
-			key: "numeric keyword applies pattern",
+			key:   "numeric keyword applies pattern",
 			value: 1234.5,
 			definition: FieldDefinition{
-				Type: "keyword",
+				Type:    "keyword",
 				Pattern: `0`,
 			},
 			fail: true,
 		},
 		// keyword and constant_keyword (other)
 		{
-			key: "bad type for keyword",
+			key:   "bad type for keyword",
 			value: map[string]interface{}{},
 			definition: FieldDefinition{
 				Type: "keyword",
@@ -119,71 +119,71 @@ func Test_parseElementValue(t *testing.T) {
 		},
 		// date
 		{
-			key: "date",
+			key:   "date",
 			value: "2020-11-02T18:01:03Z",
 			definition: FieldDefinition{
-				Type: "date",
+				Type:    "date",
 				Pattern: "^[0-9]{4}(-[0-9]{2}){2}[T ][0-9]{2}(:[0-9]{2}){2}Z$",
 			},
 		},
 		{
-			key: "bad date",
+			key:   "bad date",
 			value: "10 Oct 2020 3:42PM",
 			definition: FieldDefinition{
-				Type: "date",
+				Type:    "date",
 				Pattern: "^[0-9]{4}(-[0-9]{2}){2}[T ][0-9]{2}(:[0-9]{2}){2}Z$",
 			},
 			fail: true,
 		},
 		// ip
 		{
-			key: "ip",
+			key:   "ip",
 			value: "127.0.0.1",
 			definition: FieldDefinition{
-				Type: "ip",
+				Type:    "ip",
 				Pattern: "^[0-9.]+$",
 			},
 		},
 		{
-			key: "bad ip",
+			key:   "bad ip",
 			value: "localhost",
 			definition: FieldDefinition{
-				Type: "ip",
+				Type:    "ip",
 				Pattern: "^[0-9.]+$",
 			},
 			fail: true,
 		},
 		// text
 		{
-			key: "text",
+			key:   "text",
 			value: "some text",
 			definition: FieldDefinition{
 				Type: "text",
 			},
 		},
 		{
-			key: "text with pattern",
+			key:   "text with pattern",
 			value: "more text",
 			definition: FieldDefinition{
-				Type: "ip",
+				Type:    "ip",
 				Pattern: "[A-Z]",
 			},
 			fail: true,
 		},
 		// float
 		{
-			key: "float",
+			key:   "float",
 			value: 3.1416,
 			definition: FieldDefinition{
-				Type:        "float",
+				Type: "float",
 			},
 		},
 		// long
 		{
-			key: "bad long",
+			key:   "bad long",
 			value: "65537",
 			definition: FieldDefinition{
-				Type:        "long",
+				Type: "long",
 			},
 			fail: true,
 		},
