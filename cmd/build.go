91b373d4f18e7416f467c0c5aa7c1d4fb4d4303a
mtojek <marcin.tojek@elastic.co>
2021-01-12 11:29:49 +0100

    Address: build

--- cmd/build.go
+++ cmd/build.go
@@ -14,11 +14,11 @@ import (
 
 const buildLongDescription = `Use this command to build a package. Currently it supports only the "integration" package type.
 
-Built packages are stored in the "build/" folder located at the root folder of the local Git repository checkout that contains your package folder. The command will also render the README file if there is a corresponding template file present in "_dev/build/docs/README.md". All "_dev" directories will be omitted.
+Built packages are stored in the "build/" folder located at the root folder of the local Git repository checkout that contains your package folder. The command will also render the README file in your package folder if there is a corresponding template file present in "_dev/build/docs/README.md". All "_dev" directories under your package will be omitted.
 
 Built packages are served up by the Elastic Package Registry running locally (see "elastic-package stack"). If you want a local package to be served up by the local Elastic Package Registry, make sure to build that package first using "elastic-package build".
 
-Built packages can also be published to the "package-storage" repository.
+Built packages can also be published to the global package registry service.
 
 Context:
   package`
