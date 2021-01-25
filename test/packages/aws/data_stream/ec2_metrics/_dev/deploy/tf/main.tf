0f640983c176affe0cb0092f5d2bbc88d91472bd
mtojek <marcin.tojek@elastic.co>
2021-01-25 15:04:38 +0100

    Fix EC2 system test

--- test/packages/aws/data_stream/ec2_metrics/_dev/deploy/tf/main.tf
+++ test/packages/aws/data_stream/ec2_metrics/_dev/deploy/tf/main.tf
@@ -1,23 +1,16 @@
 provider "aws" {}
 
-resource "random_string" "id" {
-  length = 5
-  upper   = false
-  lower   = true
-  number  = false
-  special = false
-}
-
 resource "aws_instance" "i" {
-  ami           = "${data.aws_ami.latest-amzn.id}"
+  ami           = data.aws_ami.latest-amzn.id
   instance_type = "t1.micro"
   tags = {
-    Name = "elastic-package-test-${random_string.id.result}"
+    Name = "elastic-package-test"
   }
 }
 
 data "aws_ami" "latest-amzn" {
   most_recent = true
+  owners = [ "amazon" ] # AWS
   filter {
     name   = "name"
     values = ["amzn2-ami-hvm-*"]
