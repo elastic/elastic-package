a9d89f8cb7b72c965eae964c2ee58a0e7af11131
Shaunak Kashyap <ycombinator@gmail.com>
2021-01-25 18:46:05 -0800

    Adding license header + struct godoc

--- internal/testrunner/test_config.go
+++ internal/testrunner/test_config.go
@@ -1,7 +1,12 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
 package testrunner
 
 import "net/url"
 
+// SkipConfig allows a test to be marked as skipped
 type SkipConfig struct {
 	Reason string  `config:"reason"`
 	Link   url.URL `config:"url"`
