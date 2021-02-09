3f2966cdb692259fae884bd5e9e65f47600121fc
mtojek <marcin.tojek@elastic.co>
2021-02-09 15:42:22 +0100

    Try: agent 7.10.0-SNAPSHOT

--- internal/install/static_kubernetes_elastic_agent_yml.go
+++ internal/install/static_kubernetes_elastic_agent_yml.go
@@ -26,7 +26,7 @@ spec:
       serviceAccountName: kind-fleet-agent
       containers:
         - name: kind-fleet-agent-clusterscope
-          image: docker.elastic.co/beats/elastic-agent:{{ STACK_VERSION }}
+          image: docker.elastic.co/beats/elastic-agent:7.10.0-SNAPSHOT
           env:
             - name: FLEET_ENROLL
               value: "1"
