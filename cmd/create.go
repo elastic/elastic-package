7ea1a33af1493621cc61486ef25fce18e944f73d
mtojek <marcin.tojek@elastic.co>
2021-03-24 15:19:47 +0100

    Stub for subcommands

--- cmd/create.go
+++ cmd/create.go
@@ -15,18 +15,54 @@ The command can help bootstrap the first draft of a package using embedded packa
 Context:
   global, package`
 
+const createPackageLongDescription = `Use this command to create a new package.
+
+The command can bootstrap the first draft of a package using embedded package template and wizard.
+
+Context:
+  global`
+
+const createDataStreamLongDescription = `Use this command to add a new data stream to the existing package.
+
+The command can extend the package with a new data stream using a dedicated wizard.
+
+Context:
+  package`
+
 func setupCreateCommand() *cobra.Command {
+	createPackageCmd := &cobra.Command{
+		Use:   "package",
+		Short: "Create new package",
+		Long:  createPackageLongDescription,
+		RunE:  createPackageCommandAction,
+	}
+
+	createDataStreamCmd := &cobra.Command{
+		Use:   "data-stream",
+		Short: "Create new data stream",
+		Long:  createDataStreamLongDescription,
+		RunE:  createDataStreamCommandAction,
+	}
+
 	cmd := &cobra.Command{
 		Use:   "create",
 		Short: "Create package resources",
 		Long:  createLongDescription,
-		RunE:  createCommandAction,
 	}
+	cmd.AddCommand(createPackageCmd)
+	cmd.AddCommand(createDataStreamCmd)
 	return cmd
 }
 
-func createCommandAction(cmd *cobra.Command, args []string) error {
-	cmd.Println("Create package resources")
+func createPackageCommandAction(cmd *cobra.Command, args []string) error {
+	cmd.Println("Create a new package")
+
+	cmd.Println("Done")
+	return nil
+}
+
+func createDataStreamCommandAction(cmd *cobra.Command, args []string) error {
+	cmd.Println("Create a new data stream")
 
 	cmd.Println("Done")
 	return nil
