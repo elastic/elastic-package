f187867c3cb6f8aa07879dc162b819e171c2bd17
Alex K <8418476+fearful-symmetry@users.noreply.github.com>
2021-04-06 14:47:49 -0700

    Add location library to elastic-package (#303)
    * add location library to elastic-package
    
    * reove newline
    
    * migrate locations, fix formatting
    
    * remove old code from install
    
    * make fmt
    
    * write config.yml in correct location
    
    * fix locations path
    
    * fix root paths
    
    * remove duplicated functions
    
    * fix bug in install check
    
    * fix case
    
    Co-authored-by: Marcin Tojek <mtojek@users.noreply.github.com>

--- internal/stack/shellinit.go
+++ internal/stack/shellinit.go
@@ -13,7 +13,7 @@ import (
 	"gopkg.in/yaml.v3"
 
 	"github.com/elastic/elastic-package/internal/compose"
-	"github.com/elastic/elastic-package/internal/install"
+	"github.com/elastic/elastic-package/internal/configuration/locations"
 )
 
 const (
@@ -38,13 +38,13 @@ type kibanaConfiguration struct {
 
 // ShellInit method exposes environment variables that can be used for testing purposes.
 func ShellInit() (string, error) {
-	stackDir, err := install.StackDir()
+	locationManager, err := locations.NewLocationManager()
 	if err != nil {
 		return "", errors.Wrap(err, "locating stack directory failed")
 	}
 
 	// Read Elasticsearch username and password from Kibana configuration file.
-	kibanaConfigurationPath := filepath.Join(stackDir, "kibana.config.yml")
+	kibanaConfigurationPath := filepath.Join(locationManager.StackDir(), "kibana.config.yml")
 	body, err := ioutil.ReadFile(kibanaConfigurationPath)
 	if err != nil {
 		return "", errors.Wrap(err, "reading Kibana configuration file failed")
@@ -57,7 +57,7 @@ func ShellInit() (string, error) {
 	}
 
 	// Read Elasticsearch and Kibana hostnames from Elastic Stack Docker Compose configuration file.
-	p, err := compose.NewProject(DockerComposeProjectName, filepath.Join(stackDir, "snapshot.yml"))
+	p, err := compose.NewProject(DockerComposeProjectName, filepath.Join(locationManager.StackDir(), "snapshot.yml"))
 	if err != nil {
 		return "", errors.Wrap(err, "could not create docker compose project")
 	}
