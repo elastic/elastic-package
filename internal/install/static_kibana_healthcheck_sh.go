a70444c1a09740c0d06ab082a9a547028c07fe0e
Marcin Tojek <mtojek@users.noreply.github.com>
2021-04-06 13:45:46 +0200

    Update internal/install/static_kibana_healthcheck_sh.go
    Co-authored-by: Shaunak Kashyap <ycombinator@gmail.com>

--- internal/install/static_kibana_healthcheck_sh.go
+++ internal/install/static_kibana_healthcheck_sh.go
@@ -7,5 +7,5 @@ package install
 const kibanaHealthcheckSh = `#!/bin/bash
 
 curl -s -f http://127.0.0.1:5601/login | grep kbn-injected-metadata 2>&1 >/dev/null
-curl -s -f -u elastic:changeme http://elasticsearch:9200/_cat/shards | grep .security | grep STARTED
+curl -s -f -u elastic:changeme "http://elasticsearch:9200/_cat/indices/.security-*?h=health" | grep -v red
 `
