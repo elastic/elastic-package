b705fbea3c46e7453a0588f0f93dfbbeda56f33c
mtojek <marcin.tojek@elastic.co>
2021-04-20 17:01:25 +0200

    More files

--- internal/packages/archetype/package_docs_readme.go
+++ internal/packages/archetype/package_docs_readme.go
@@ -4,8 +4,8 @@
 
 package archetype
 
-const packageDocs = `# {{.Title}}
+const packageDocsReadme = `# {{.Title}}
 
 This is a new integration created using the [elastic-package](https://github.com/elastic/elastic-package) tool.
 
-Consider using the README template file ` + "`_dev/docs/README.md`" + `to generate a list of exported fields.`
+Consider using the README template file ` + "`_dev/docs/README.md`" + `to generate a list of exported fields or include a sample event.`
