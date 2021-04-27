1f24d30802998c39300a687d543be3c2188002f3
ruflin <spam@ruflin.com>
2021-04-27 19:49:45 +0200

    [Testing] Test if fleet-server policy by id is picked up

--- internal/install/static_kibana_config_yml.go
+++ internal/install/static_kibana_config_yml.go
@@ -21,4 +21,59 @@ xpack.fleet.agents.elasticsearch.host: "http://elasticsearch:9200"
 xpack.fleet.agents.fleet_server.hosts: ["http://fleet-server:8220"]
 
 xpack.encryptedSavedObjects.encryptionKey: "12345678901234567890123456789012"
+
+# CLOUD CONFIG
+xpack.fleet.packages:
+  - name: fleet_server
+    version: latest
+  - name: system
+    version: latest
+
+xpack.fleet.agentPolicies:
+  # Cloud Agent policy
+  - name: Elastic Cloud agent policy
+    description: Default agent policy for agents hosted on Elastic Cloud
+    is_managed: true
+    #id: elastic-agent-on-cloud
+    id: 5a12d34e-0162-4c2b-9f57-841bf90ffb02
+    is_default_fleet_server: false
+    monitoring_enabled: []
+    package_policies:
+      - package:
+          name: fleet_server
+        name: Fleet Server
+        inputs:
+          - type: fleet-server
+            vars:
+              - name: host
+                value: 0.0.0.0
+                frozen: true
+
+  # Default policy
+  - name: Default policy
+    namespace: default
+    description: Default agent policy created by Kibana
+    is_default: true
+    is_managed: false
+    monitoring_enabled:
+      - logs
+      - metrics
+    package_policies:
+      - name: system-1
+        package:
+          name: system
+
+  # Default Fleet Server policy
+  - name: Default Fleet Server policy
+    description: Default Fleet Server agent policy created by Kibana
+    is_managed: false
+    is_default_fleet_server: true
+    is_default: false
+    monitoring_enabled:
+      - logs
+      - metrics
+    package_policies:
+      - package:
+          name: fleet_server
+        name: fleet_server-1
 `
