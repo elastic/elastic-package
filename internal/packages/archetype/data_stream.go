32d8a2f1b33e894fdb4b4ae0de440175b34f61fc
mtojek <marcin.tojek@elastic.co>
2021-04-28 16:16:01 +0200

    Define archetype for data stream

---
+++ internal/packages/archetype/data_stream.go
@@ -0,0 +1,12 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package archetype
+
+import "github.com/elastic/elastic-package/internal/packages"
+
+// DataStreamDescriptor defines configurable properties of the data stream archetype
+type DataStreamDescriptor struct {
+	Manifest packages.DataStreamManifest
+}
