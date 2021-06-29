28a160e74e6ec69e2d8a6c75e0e85e7f6de3715d
René Kalff <r.kalff@intermax.nl>
2021-06-29 08:32:01 +0000

    Leave system testrunner teardown to main function

--- internal/testrunner/runners/system/runner.go
+++ internal/testrunner/runners/system/runner.go
@@ -162,9 +162,6 @@ func (r *runner) run() (results []testrunner.TestResult, err error) {
 		if err != nil {
 			return results, err
 		}
-		if err = r.TearDown(); err != nil {
-			return results, errors.Wrap(err, "failed to teardown runner")
-		}
 	}
 	return results, nil
 }
