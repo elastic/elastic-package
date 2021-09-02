e4c49e95bc91f20c70c97c728f154c5882d6f175
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2021-09-02 19:29:49 +0200

    Use embed for install static files

--- internal/install/_static/terraform_deployer_run.sh
+++ internal/install/_static/terraform_deployer_run.sh
@@ -1,10 +1,4 @@
-// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
-// or more contributor license agreements. Licensed under the Elastic License;
-// you may not use this file except in compliance with the Elastic License.
-
-package install
-
-const terraformDeployerRun = `#!sh
+#!sh
 
 set -euxo pipefail
 
