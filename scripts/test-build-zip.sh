27fa9c93e0ced5c86f823bc8109699060611e466
mtojek <marcin.tojek@elastic.co>
2021-10-21 18:07:31 +0200

    CI: introduce build-zip goal

---
+++ scripts/test-build-zip.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+cleanup() {
+  r=$?
+
+  # Dump stack logs
+  elastic-package stack dump -v --output build/elastic-stack-dump/build-zip
+
+  # Take down the stack
+  elastic-package stack down -v
+
+  # Clean used resources
+  for d in test/packages/*/; do
+    (
+      cd $d
+      elastic-package clean -v
+    )
+  done
+
+  exit $r
+}
+
+trap cleanup EXIT
+
+OLDPWD=$PWD
+# Build packages
+for d in test/packages/*/; do
+  (
+    cd $d
+    elastic-package build --zip -v
+  )
+done
+cd -
+
+# Update the stack
+elastic-package stack update -v --services package-registry
+
+# Boot up the stack
+elastic-package stack up -d -v --services package-registry
+
+cd -
+done
