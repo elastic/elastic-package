7911ff70d9bfd157544892d5f8b748b122b09b21
mtojek <marcin.tojek@elastic.co>
2021-10-26 12:05:15 +0200

    Test boot up of different stack versions

--- scripts/test-stack-command.sh
+++ scripts/test-stack-command.sh
@@ -2,11 +2,17 @@
 
 set -euxo pipefail
 
+VERSION=${1}
+
+if [ "${VERSION}" == "" ]; then
+  echo "stack version required" && exit 1
+fi
+
 cleanup() {
   r=$?
 
   # Dump stack logs
-  elastic-package stack dump -v --output build/elastic-stack-dump/stack
+  elastic-package stack dump -v --output build/elastic-stack-dump/stack/${VERSION}
 
   # Take down the stack
   elastic-package stack down -v
@@ -25,10 +31,10 @@ cleanup() {
 trap cleanup EXIT
 
 # Update the stack
-elastic-package stack update -v
+elastic-package stack update --version ${VERSION} -v
 
 # Boot up the stack
-elastic-package stack up -d -v
+elastic-package stack up -d --version ${VERSION} -v
 
 # Verify it's accessible
 eval "$(elastic-package stack shellinit)"
