7fadec35c4a75d2f49b98e78ab57551cdf3e0caa
mtojek <marcin.tojek@elastic.co>
2021-12-15 16:30:32 +0100

    Fix

--- scripts/test-check-packages.sh
+++ scripts/test-check-packages.sh
@@ -6,7 +6,7 @@ cleanup() {
   r=$?
 
   # Dump stack logs
-  elastic-package stack dump -v --output build/elastic-stack-dump/check
+  elastic-package stack dump -v --output "build/elastic-stack-dump/check-${PACKAGE_UNDER_TEST:-any}"
 
   if [ "${PACKAGE_TEST_TYPE:-other}" == "with-kind" ]; then
     # Dump kubectl details
