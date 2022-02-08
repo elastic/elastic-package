8bc1d37e55c584c1e97bce30a0f51a8e61c23bef
Edoardo Tenani <edoardo.tenani@elastic.co>
2022-02-08 11:26:54 +0100

    add gcp.billing tests

---
+++ test/packages/parallel/gcp/data_stream/billing/_dev/deploy/tf/.terraform.lock.hcl
@@ -0,0 +1,38 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/google" {
+  version = "4.1.0"
+  hashes = [
+    "h1:oXcSMwbjSCr0PO2PyZbKPbPXnd4EhxdJGZQwtzQqwrM=",
+    "zh:3477415ef4ef02d7c065a08fb24b7011ddd77f76b2a011db25e0b73c767acaae",
+    "zh:43be16d0bbb56d29c090c1abe49e877e4f3ca66bb372a661a2cd0a377cd61e8f",
+    "zh:6bb202a40055ed302f9dd05e2f3f87aa13080e580a9ac7b0241d634b78b68a2a",
+    "zh:7179303f628c5fc58129013ef4a4e754e225e266e68af4a7167a06a64d9d97db",
+    "zh:837cdcf1bafd4c936f15a4a22353efeec96ca23727f8d8ff22e9d53414d87084",
+    "zh:a853f1a1e9f5ac261fd9a02d869ce7da16917fdd16464dabf2200e2ccd04fba9",
+    "zh:aba631b9db47cc34857c25459475f17d939a46022f886e396a8254588f720c58",
+    "zh:ac461358984ba480b4fef1d2be3538d9714aa8814a6f420e1e954227054a14a4",
+    "zh:ade64689485fc28b6aa0c36861c6c9c66b5d7a8e7282739ecf97c7791b973312",
+    "zh:c3fbbfe7f6e870e19bb552a2ad84ea38c9ddf3e25e0b7e996ecb2fc7ce336b03",
+    "zh:d1419f4fe9c07f136ee9015a299cd6acf25a13bea0afa0f69727a576058603d3",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/local" {
+  version = "2.1.0"
+  hashes = [
+    "h1:EYZdckuGU3n6APs97nS2LxZm3dDtGqyM4qaIvsmac8o=",
+    "zh:0f1ec65101fa35050978d483d6e8916664b7556800348456ff3d09454ac1eae2",
+    "zh:36e42ac19f5d68467aacf07e6adcf83c7486f2e5b5f4339e9671f68525fc87ab",
+    "zh:6db9db2a1819e77b1642ec3b5e95042b202aee8151a0256d289f2e141bf3ceb3",
+    "zh:719dfd97bb9ddce99f7d741260b8ece2682b363735c764cac83303f02386075a",
+    "zh:7598bb86e0378fd97eaa04638c1a4c75f960f62f69d3662e6d80ffa5a89847fe",
+    "zh:ad0a188b52517fec9eca393f1e2c9daea362b33ae2eb38a857b6b09949a727c1",
+    "zh:c46846c8df66a13fee6eff7dc5d528a7f868ae0dcf92d79deaac73cc297ed20c",
+    "zh:dc1a20a2eec12095d04bf6da5321f535351a594a636912361db20eb2a707ccc4",
+    "zh:e57ab4771a9d999401f6badd8b018558357d3cbdf3d33cc0c4f83e818ca8e94b",
+    "zh:ebdcde208072b4b0f8d305ebf2bfdc62c926e0717599dcf8ec2fd8c5845031c3",
+    "zh:ef34c52b68933bedd0868a13ccfd59ff1c820f299760b3c02e008dc95e2ece91",
+  ]
+}
---
+++ test/packages/parallel/gcp/data_stream/billing/_dev/deploy/tf/main.tf
@@ -0,0 +1,77 @@
+provider "google" {
+  project = var.project_id
+}
+
+provider "local" {}
+
+resource "google_bigquery_dataset" "testing" {
+  dataset_id                  = "integration_testing_${var.TEST_RUN_ID}"
+  friendly_name               = "GCP Integration testing"
+  description                 = "Used for testing the gcp integration package"
+  location                    = "EU"
+  default_table_expiration_ms = 3600000
+
+  labels = {
+    team   = "integrations"
+    env    = "test"
+    run_id = var.TEST_RUN_ID
+  }
+}
+
+
+# Creates a local file with useful test data for gcp.billing data stream.
+# The underlying metricbeat metricset queries based on current year and month,
+# so this file, which is then uploaded to BigQuery table, should have current
+# year and month.
+# NOTE: project.id must not be NULL (used to filter results)
+# NOTE: project.name must not be NULL (used to build eventID)
+# NOTE: when this resource content changes, google_bigquery_table.default should be tainted
+resource "local_file" "bq_test_data" {
+  content = templatefile("${path.root}/${var.test_data_file}.tftpl", {
+    ymd           = formatdate("YYYY-MM-DD", timestamp())
+    invoice_month = formatdate("YYYYMM", timestamp())
+  })
+
+  filename = "${path.root}/${var.test_data_file}"
+
+  file_permission = "0660"
+}
+
+resource "google_bigquery_table" "default" {
+  dataset_id = google_bigquery_dataset.testing.dataset_id
+  table_id   = "billing_export_test_data_${var.TEST_RUN_ID}"
+
+  deletion_protection = false
+
+  # NOTE: generation of test data file is required, as the file is used in the 
+  # local-exec provisioner where there are no dependency checks.
+  # NOTE: when this resource content changes the provisioner is not run 
+  # automatically.
+  depends_on = [local_file.bq_test_data]
+
+  time_partitioning {
+    type = "DAY"
+  }
+
+  labels = {
+    team   = "integrations"
+    env    = "test"
+    run_id = var.TEST_RUN_ID
+  }
+
+  schema = file("${path.root}/${var.billing_biquery_schema_file}")
+
+  # use local-exec to run a go script leveraging BigQuery SDK to upload test data to the table
+  # using go as is cross-platform
+  provisioner "local-exec" {
+    command = <<EOT
+bq --location=EU load \
+  --source_format=NEWLINE_DELIMITED_JSON \
+  --project_id=${var.project_id} \
+  ${google_bigquery_dataset.testing.dataset_id}.${google_bigquery_table.default.table_id} \
+  "${path.root}/${var.test_data_file}" \
+  "${path.root}/${var.billing_biquery_schema_file}"
+EOT
+  }
+
+}
