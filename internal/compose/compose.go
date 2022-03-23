867e842b4f7ff09888f4e51eaf1c97a155c8cd91
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2022-03-23 11:08:08 +0100

    Handle string ports in docker compose v2 config yaml (#748)
    docker-compose v2.3.x changed some ports in `docker-compose config` from
    int to string. Handle both cases.

--- internal/compose/compose.go
+++ internal/compose/compose.go
@@ -54,6 +54,20 @@ type portMapping struct {
 	Protocol     string
 }
 
+type intOrStringYaml int
+
+func (p *intOrStringYaml) UnmarshalYAML(node *yaml.Node) error {
+	var s string
+	err := node.Decode(&s)
+	if err == nil {
+		i, err := strconv.Atoi(s)
+		*p = intOrStringYaml(i)
+		return err
+	}
+
+	return node.Decode(p)
+}
+
 // UnmarshalYAML unmarshals a Docker Compose port mapping in YAML to
 // a portMapping.
 func (p *portMapping) UnmarshalYAML(node *yaml.Node) error {
@@ -67,9 +81,9 @@ func (p *portMapping) UnmarshalYAML(node *yaml.Node) error {
 		}
 
 		var s struct {
-			HostIP    string `yaml:"host_ip"`
-			Target    int
-			Published int
+			HostIP    string          `yaml:"host_ip"`
+			Target    intOrStringYaml // Docker compose v2 can define ports as strings.
+			Published intOrStringYaml // Docker compose v2 can define ports as strings.
 			Protocol  string
 		}
 
@@ -77,8 +91,8 @@ func (p *portMapping) UnmarshalYAML(node *yaml.Node) error {
 			return errors.Wrap(err, "could not unmarshal YAML map node")
 		}
 
-		p.InternalPort = s.Target
-		p.ExternalPort = s.Published
+		p.InternalPort = int(s.Target)
+		p.ExternalPort = int(s.Published)
 		p.Protocol = s.Protocol
 		p.ExternalIP = s.HostIP
 		return nil
