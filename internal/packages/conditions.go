093d1ddff8a1d0b3aa807f1117416ea9eb950781
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2022-12-19 15:39:26 +0100

    Bulk update of several dependencies

--- internal/packages/conditions.go
+++ internal/packages/conditions.go
@@ -8,7 +8,7 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/Masterminds/semver"
+	"github.com/Masterminds/semver/v3"
 	"github.com/pkg/errors"
 
 	"github.com/elastic/elastic-package/internal/logger"
