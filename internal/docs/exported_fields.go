c62cf0329f60e2de312da19ce6f82385d58740fb
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2023-01-13 12:25:46 +0100

    Remove redundant ValidatorOption from exported_fields

--- internal/docs/exported_fields.go
+++ internal/docs/exported_fields.go
@@ -25,8 +25,7 @@ type fieldsTableRecord struct {
 var escaper = strings.NewReplacer("*", "\\*", "{", "\\{", "}", "\\}", "<", "\\<", ">", "\\>")
 
 func renderExportedFields(fieldsParentDir string) (string, error) {
-	validator, err := fields.CreateValidatorForDirectory(fieldsParentDir,
-		fields.WithEnabledImportAllECSSChema(false))
+	validator, err := fields.CreateValidatorForDirectory(fieldsParentDir)
 	if err != nil {
 		return "", errors.Wrapf(err, "can't create fields validator instance (path: %s)", fieldsParentDir)
 	}
