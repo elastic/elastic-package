86f11a2bf147ce9c4fdc8a370ae24006af93bf08
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2023-01-29 13:58:28 +0100

    Retry to boot the stack up if only elastic-agent failed to start

--- internal/stack/boot.go
+++ internal/stack/boot.go
@@ -60,10 +60,37 @@ func BootUp(options Options) error {
 
 	err = dockerComposeUp(options)
 	if err != nil {
+		// At least starting on 8.6.0, fleet-server may be reconfigured or
+		// restarted after being healthy. If elastic-agent tries to enroll at
+		// this moment, it fails inmediately and makes `docker-compose up` to fail too.
+		// As workaround, try to give another chance to docker-compose if only
+		// elastic-agent failed.
+		if onlyElasticAgentFailed() {
+			err = dockerComposeUp(options)
+		}
 		return errors.Wrap(err, "running docker-compose failed")
 	}
 
 	return nil
+}
+
+func onlyElasticAgentFailed() bool {
+	status, err := Status()
+	if err != nil {
+		fmt.Println("Failed to check status of the stack after failure: %s", err)
+		return false
+	}
+
+	for _, service := range status {
+		if strings.Contains(service.Name, "elastic-agent") {
+			continue
+		}
+		if !strings.HasPrefix(service.Status, "running") {
+			return false
+		}
+	}
+
+	return true
 }
 
 // TearDown function takes down the testing stack.
