6f2a1881b47fb34cf854653d7374171dc6077b93
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2023-02-02 11:36:39 +0100

    Add more stages into integration tests

--- .buildkite/pipeline.trigger.integration.tests.sh
+++ .buildkite/pipeline.trigger.integration.tests.sh
@@ -11,8 +11,11 @@ echo "    steps:"
 
 # Integration tests related stack command
 STACK_COMMAND_TESTS=(
-  test-stack-command-default
-  test-stack-command-oldest
+    test-stack-command-default
+    test-stack-command-oldest
+    test-stack-command-7x
+    test-stack-command-86
+    test-stack-command-8x
 )
 
 for test in ${STACK_COMMAND_TESTS[@]}; do
@@ -25,3 +28,36 @@ for test in ${STACK_COMMAND_TESTS[@]}; do
     echo "          - build/elastic-stack-dump/stack/*/logs/fleet-server-internal/*.log"
     echo "          - build/elastic-stack-status/*/*"
 done
+
+CHECK_PACKAGES_TESTS=(
+    test-check-packages-other
+    test-check-packages-with-kind
+    test-check-packages-with-custom-agent
+    test-check-packages-benchmarks
+)
+for test in ${CHECK_PACKAGES_TESTS[@]}; do
+    echo "      - label: \":go: Running integration test: ${test}\""
+    echo "        command: ./.buildkite/scripts/integration_tests.sh -t ${test}"
+    echo "        agents:"
+    echo "          provider: \"gcp\""
+    echo "        artifact_paths:"
+    echo "          - build/test-results/*.xml"
+    echo "          - build/elastic-stack-dump/stack/*/logs/*.log"
+    echo "          - build/elastic-stack-dump/stack/*/logs/fleet-server-internal/*.log"
+    if [[ $test =~ with-kind ]]; then
+        echo "          - build/kubectl-dump.txt"
+    fi
+done
+
+echo "      - label: \":go: Running integration test: test-build-zip\""
+echo "        command: ./.buildkite/scripts/integration_tests.sh -t test-build-zip"
+echo "        agents:"
+echo "          provider: \"gcp\""
+echo "        artifact_paths:"
+echo "          - build/elastic-stack-dump/stack/*/logs/*.log"
+echo "          - build/packages/*.sig"
+
+echo "      - label: \":go: Running integration test: test-profiles-command\""
+echo "        command: ./.buildkite/scripts/integration_tests.sh -t test-profiles-command"
+echo "        agents:"
+echo "          provider: \"gcp\""
