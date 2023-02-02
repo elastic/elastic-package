663bab8205cade441d7eb9392e936113df04bd68
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2023-02-02 11:06:11 +0100

    Fix trigger pipeline script

--- .buildkite/pipeline.trigger.integration.tests.sh
+++ .buildkite/pipeline.trigger.integration.tests.sh
@@ -5,20 +5,23 @@ set -eu
 
 # begin the pipeline.yml file
 echo "steps:"
+echo "  - group: \":terminal: Integration test suite\""
+echo "    key: \"integration-tests\""
+echo "    steps:"
 
 # Integration tests related stack command
-STACK_COMMAND_TESTS = (
+STACK_COMMAND_TESTS=(
   test-stack-command-default
   test-stack-command-oldest
 )
 
 for test in ${STACK_COMMAND_TESTS[@]}; do
-    echo "  - label: :go: Running integration test: ${test}"
-    echo "    command: ./buildkite/scripts/integration_tests.sh -t ${test}"
-    echo "    agents:"
-    echo "      provider: \"gcp\""
-    echo "    artifact_paths:"
-    echo "      - build/elastic-stack-dump/stack/*/logs/*.log"
-    echo "      - build/elastic-stack-dump/stack/*/logs/fleet-server-internal/*.log"
-    echo "      - build/elastic-stack-status/*/*"
+    echo "      - label: \":go: Running integration test: ${test}\""
+    echo "        command: ./.buildkite/scripts/integration_tests.sh -t ${test}"
+    echo "        agents:"
+    echo "          provider: \"gcp\""
+    echo "        artifact_paths:"
+    echo "          - build/elastic-stack-dump/stack/*/logs/*.log"
+    echo "          - build/elastic-stack-dump/stack/*/logs/fleet-server-internal/*.log"
+    echo "          - build/elastic-stack-status/*/*"
 done
