0051f5b621db83b62d53e211e629c92c1b299dd3
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2023-02-15 19:07:55 +0100

    More tests

--- .buildkite/scripts/test_secret.sh
+++ .buildkite/scripts/test_secret.sh
@@ -8,3 +8,6 @@ echo "${MY_SECRET}" > some_file
 echo "File:"
 cat some_file
 echo "Is it shown using other env. var? ${OTHER_VAR}"
+echo "Other vars"
+echo " - FOO_VAR => ${FOO_VAR}"
+echo " - TEST_VAR => ${TEST_VAR}"
