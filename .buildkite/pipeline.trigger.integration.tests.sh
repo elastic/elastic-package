79ee1738578deac17f8f5caf8d925db6244ab6df
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2023-02-16 15:55:53 +0100

    Add more stages to main buildkite pipeline (#1130)
    Add the missing steps into buildkite pipeline (release and integration tests
    steps). Integration tests require to use GCP agents to be able to use
    docker-like commands.
    It has also been configured a post-checkout hook to ensure that
    the build will run against what the merged commit will be, rather
    than what is in the PR.

---
+++ .buildkite/pipeline.trigger.integration.tests.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# exit immediately on failure, or if an undefined variable is used
+set -eu
+
+# begin the pipeline.yml file
+echo "steps:"
+echo "  - group: \":terminal: Integration test suite\""
+echo "    key: \"integration-tests\""
+echo "    steps:"
+
+# Integration tests related stack command
+STACK_COMMAND_TESTS=(
+    test-stack-command-default
+    test-stack-command-oldest
+    test-stack-command-7x
+    test-stack-command-86
+    test-stack-command-8x
+)
+
+for test in ${STACK_COMMAND_TESTS[@]}; do
+    echo "      - label: \":go: Running integration test: ${test}\""
+    echo "        command: ./.buildkite/scripts/integration_tests.sh -t ${test}"
+    echo "        agents:"
+    echo "          provider: \"gcp\""
+    echo "        artifact_paths:"
+    echo "          - build/elastic-stack-dump/stack/*/logs/*.log"
+    echo "          - build/elastic-stack-dump/stack/*/logs/fleet-server-internal/*.log"
+    echo "          - build/elastic-stack-status/*/*"
+done
+
+CHECK_PACKAGES_TESTS=(
+    test-check-packages-other
+    test-check-packages-with-kind
+    test-check-packages-with-custom-agent
+    test-check-packages-benchmarks
+)
+for test in ${CHECK_PACKAGES_TESTS[@]}; do
+    echo "      - label: \":go: Running integration test: ${test}\""
+    echo "        command: ./.buildkite/scripts/integration_tests.sh -t ${test}"
+    echo "        agents:"
+    echo "          provider: \"gcp\""
+    echo "        artifact_paths:"
+    echo "          - build/test-results/*.xml"
+    echo "          - build/elastic-stack-dump/stack/check-*/logs/*.log"
+    echo "          - build/elastic-stack-dump/stack/check-*/logs/fleet-server-internal/*.log"
+    if [[ $test =~ with-kind$ ]]; then
+        echo "          - build/kubectl-dump.txt"
+    fi
+done
+
+pushd test/packages/parallel > /dev/null
+for package in $(find . -maxdepth 1 -mindepth 1 -type d) ; do
+    package_name=$(basename ${package})
+    echo "      - label: \":go: Running integration test: ${package_name}\""
+    echo "        key: \"integration-parallel-${package_name}\""
+    echo "        command: ./.buildkite/scripts/integration_tests.sh -t test-check-packages-parallel -p ${package_name}"
+    echo "        agents:"
+    echo "          provider: \"gcp\""
+    echo "        artifact_paths:"
+    echo "          - build/test-results/*.xml"
+    echo "          - build/elastic-stack-dump/stack/check-*/logs/*.log"
+    echo "          - build/elastic-stack-dump/stack/check-*/logs/fleet-server-internal/*.log"
+done
+
+popd > /dev/null
+
+echo "      - label: \":go: Running integration test: test-build-zip\""
+echo "        command: ./.buildkite/scripts/integration_tests.sh -t test-build-zip"
+echo "        agents:"
+echo "          provider: \"gcp\""
+echo "        artifact_paths:"
+echo "          - build/elastic-stack-dump/stack/*/logs/*.log"
+echo "          - build/packages/*.sig"
+
+echo "      - label: \":go: Running integration test: test-profiles-command\""
+echo "        command: ./.buildkite/scripts/integration_tests.sh -t test-profiles-command"
+echo "        agents:"
+echo "          provider: \"gcp\""
