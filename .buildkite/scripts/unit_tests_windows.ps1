89ef17adb52ddd643078f417f688cb3f72fa0f23
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2023-03-06 12:51:33 +0100

    Use gcp provider with windows image
    Co-Authored-By: Mario Rodriguez Molins <mario.rodriguez@elastic.co>

---
+++ .buildkite/scripts/unit_tests_windows.ps1
@@ -0,0 +1,24 @@
+echo "--- Fixing CRLF in git checkout"
+# Forcing to checkout again all the files with a correct autocrlf.
+# Doing this here because we cannot set git clone options before.
+git config core.autocrlf input
+git rm --quiet --cached -r .
+git reset --quiet --hard
+
+echo "--- Installing golang"
+choco install -y golang --version 1.19.5
+
+echo "--- Updating session environment"
+# refreshenv requires to have chocolatey profile installed
+$env:ChocolateyInstall = Convert-Path "$((Get-Command choco).Path)\..\.."
+Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+
+refreshenv
+
+echo "--- Downloading Go modules"
+go version
+go mod download -x
+
+echo "--- Running unit tests"
+go version
+go test ./...
