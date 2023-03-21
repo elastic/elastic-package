ac83e7c60c519ea707f49deb14ec64fc7036f6a2
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2023-03-21 17:11:41 +0100

    Restore pipeline and release step

--- .buildkite/scripts/release.sh
+++ .buildkite/scripts/release.sh
@@ -23,38 +23,4 @@ git fetch origin --tags
 
 echo "--- running goreleaser"
 # Run latest version of goreleaser
-# curl -sL https://git.io/goreleaser | bash
-
-TAR_FILE="/tmp/goreleaser.tar.gz"
-RELEASES_URL="https://github.com/goreleaser/goreleaser/releases"
-# test -z "$TMPDIR" && TMPDIR="$(mktemp -d)"
-TARGET_DIR="${WORKSPACE}/bin"
-
-last_version() {
-  curl -sL -o /dev/null -w %{url_effective} "$RELEASES_URL/latest" |
-    rev |
-    cut -f1 -d'/'|
-    rev
-}
-
-download() {
-  test -z "$VERSION" && VERSION="$(last_version)"
-  test -z "$VERSION" && {
-    echo "Unable to get goreleaser version." >&2
-    exit 1
-  }
-  rm -f "$TAR_FILE"
-  curl -s -L -o "$TAR_FILE" \
-    "$RELEASES_URL/download/$VERSION/goreleaser_$(uname -s)_$(uname -m).tar.gz"
-}
-
-download
-tar -xf "$TAR_FILE" -C "${TARGET_DIR}"
-
-rm ${TAR_FILE}
-chmod u+x ${TARGET_DIR}/goreleaser
-
-git status
-
-# release skip
-goreleaser release --skip-publish --snapshot
+curl -sL https://git.io/goreleaser | bash
