e4a0b68fc0b0b997d10cc28d90e2f28e298cc0b2
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2023-04-05 16:47:01 +0200

    Remove fleet managed tags (#1206)
    Remove fleet managed tags from kibana resources as well
    as remove the Kibana tag objects related to those tags when
    running "elastic-package export dashboards" command.

---
+++ internal/export/transform_remove_fleet_managed_tags.go
@@ -0,0 +1,113 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package export
+
+import (
+	"fmt"
+
+	"github.com/elastic/elastic-package/internal/common"
+)
+
+func removeFleetManagedTags(ctx *transformationContext, object common.MapStr) (common.MapStr, error) {
+	aType, err := object.GetValue("type")
+	if err != nil {
+		return nil, fmt.Errorf("failed to read type field: %w", err)
+	}
+
+	if aType == "tag" {
+		return removeTagObjects(ctx, object)
+	}
+
+	return removeTagReferences(ctx, object)
+}
+
+func removeTagReferences(ctx *transformationContext, object common.MapStr) (common.MapStr, error) {
+	references, err := object.GetValue("references")
+	if err == common.ErrKeyNotFound {
+		return object, nil
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to read references field: %w", err)
+	}
+
+	newReferences, err := filterOutFleetManagedTags(ctx, references.([]interface{}))
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = object.Put("references", newReferences)
+	if err != nil {
+		return nil, fmt.Errorf("can't update references: %w", err)
+	}
+
+	return object, nil
+}
+
+func removeTagObjects(ctx *transformationContext, object common.MapStr) (common.MapStr, error) {
+	aId, err := object.GetValue("id")
+	if err == common.ErrKeyNotFound {
+		return object, nil
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to read id field: %w", err)
+	}
+
+	aIdString, ok := aId.(string)
+	if !ok {
+		return nil, fmt.Errorf("failed to assert id as a string: %v", aId)
+	}
+
+	if isTagFleetManaged(aIdString, ctx.packageName) {
+		return nil, nil
+	}
+	return object, nil
+}
+
+func isTagFleetManaged(aId, packageName string) bool {
+	var empty interface{}
+	fleetManagedTags := map[string]interface{}{
+		fmt.Sprintf("fleet-pkg-%s-default", packageName):                 empty,
+		"fleet-managed-default":                                          empty,
+		fmt.Sprintf("%s-fleet-pkg-%s-default", packageName, packageName): empty,
+		fmt.Sprintf("%s-fleet-managed-default", packageName):             empty,
+	}
+
+	_, ok := fleetManagedTags[aId]
+	return ok
+}
+
+func filterOutFleetManagedTags(ctx *transformationContext, references []interface{}) ([]interface{}, error) {
+	newReferences := make([]interface{}, 0)
+	for _, r := range references {
+		reference := r.(map[string]interface{})
+
+		aType, ok := reference["type"]
+		if !ok {
+			continue
+		}
+		if aType != "tag" {
+			newReferences = append(newReferences, r)
+			continue
+		}
+
+		aId, ok := reference["id"]
+		if !ok {
+			newReferences = append(newReferences, r)
+			continue
+		}
+
+		aIdString, ok := aId.(string)
+		if !ok {
+			return nil, fmt.Errorf("failed to assert id as a string: %v", aId)
+		}
+		if isTagFleetManaged(aIdString, ctx.packageName) {
+			continue
+		}
+		newReferences = append(newReferences, r)
+	}
+	return newReferences, nil
+}
---
+++ internal/export/transform_remove_fleet_managed_tags_test.go
@@ -0,0 +1,87 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package export
+
+import (
+	"encoding/json"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/elastic/elastic-package/internal/common"
+)
+
+func TestRemoveFleetTagsDashboard(t *testing.T) {
+	b, err := os.ReadFile("./testdata/elastic_package_registry.dashboard.json")
+	require.NoError(t, err)
+
+	var given common.MapStr
+	err = json.Unmarshal(b, &given)
+	require.NoError(t, err)
+
+	ctx := &transformationContext{
+		packageName: "elastic_package_registry",
+	}
+
+	result, err := removeFleetManagedTags(ctx, given)
+	require.NoError(t, err)
+
+	resultJson, err := json.MarshalIndent(&result, "", "    ")
+	require.NoError(t, err)
+
+	expected, err := os.ReadFile("./test/elastic_package_registry.dashboard.json-expected.json")
+	require.NoError(t, err)
+
+	require.Equal(t, string(expected), string(resultJson))
+}
+
+func TestRemoveFleetTagsObjects(t *testing.T) {
+	cases := []struct {
+		title           string
+		objectFile      string
+		expectedRemoved bool
+	}{
+		{
+			title:           "Tag managed by fleet",
+			objectFile:      "./testdata/elastic_package_registry.tag_managed_by_fleet.json",
+			expectedRemoved: true,
+		},
+		{
+			title:           "Random tag",
+			objectFile:      "./testdata/elastic_package_registry.random_tag.json",
+			expectedRemoved: false,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.title, func(t *testing.T) {
+			b, err := os.ReadFile(c.objectFile)
+			require.NoError(t, err)
+
+			var given common.MapStr
+			err = json.Unmarshal(b, &given)
+			require.NoError(t, err)
+
+			ctx := &transformationContext{
+				packageName: "elastic_package_registry",
+			}
+
+			result, err := removeFleetManagedTags(ctx, given)
+			require.NoError(t, err)
+
+			if c.expectedRemoved {
+				assert.Nil(t, result)
+				return
+			}
+
+			resultJson, err := json.MarshalIndent(&result, "", "    ")
+			require.NoError(t, err)
+
+			assert.Equal(t, string(b), string(resultJson))
+		})
+	}
+}
