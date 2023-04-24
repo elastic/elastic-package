c40e3f412b8646cec0615c75972e5f4e256b5c4b
Marc Guasch <marc.guasch@elastic.co>
2023-04-24 12:37:06 +0200

    Add monitoringenabled option

--- internal/testrunner/runners/system/runner.go
+++ internal/testrunner/runners/system/runner.go
@@ -340,9 +340,10 @@ func (r *runner) runTest(config *testConfig, ctxt servicedeployer.ServiceContext
 	logger.Debug("creating test policy...")
 	testTime := time.Now().Format("20060102T15:04:05Z")
 	p := kibana.Policy{
-		Name:        fmt.Sprintf("ep-test-system-%s-%s-%s", r.options.TestFolder.Package, r.options.TestFolder.DataStream, testTime),
-		Description: fmt.Sprintf("test policy created by elastic-package test system for data stream %s/%s", r.options.TestFolder.Package, r.options.TestFolder.DataStream),
-		Namespace:   "ep",
+		Name:              fmt.Sprintf("ep-test-system-%s-%s-%s", r.options.TestFolder.Package, r.options.TestFolder.DataStream, testTime),
+		Description:       fmt.Sprintf("test policy created by elastic-package test system for data stream %s/%s", r.options.TestFolder.Package, r.options.TestFolder.DataStream),
+		Namespace:         "ep",
+		MonitoringEnabled: []string{"logs", "metrics"},
 	}
 	policy, err := kib.CreatePolicy(p)
 	if err != nil {
