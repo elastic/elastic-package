c137cbb29c8d0bf305ca038717cee358c77f5003
Andrea Spacca <andrea.spacca@elastic.co>
2023-05-18 12:00:24 +0900

    refatoring for client mocking

--- cmd/benchmark.go
+++ cmd/benchmark.go
@@ -269,7 +269,8 @@ func generateDataStreamCorpusCommandAction(cmd *cobra.Command, _ []string) error
 		return cobraext.FlagParsingError(err, cobraext.GenerateCorpusRallyTrackOutputDirFlagName)
 	}
 
-	generator, err := corpusgenerator.NewGenerator(packageName, dataSetName, commit, totSizeInBytes)
+	genLibClient := corpusgenerator.NewClient(commit)
+	generator, err := corpusgenerator.NewGenerator(genLibClient, packageName, dataSetName, totSizeInBytes)
 	if err != nil {
 		return errors.Wrap(err, "can't generate benchmarks data corpus for data stream")
 	}
