0fc616dd0b77e2c5cbb8ce8f8ea9427452ffda90
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2023-05-23 17:46:27 +0200

    Update test-with-integrations pipeline

--- .buildkite/scripts/install_deps.sh
+++ .buildkite/scripts/install_deps.sh
@@ -39,3 +39,28 @@ with_docker_compose() {
     chmod +x ${WORKSPACE}/bin/docker-compose
     docker-compose version
 }
+
+with_github_cli() {
+    mkdir -p ${WORKSPACE}/bin
+    mkdir -p ${WORKSPACE}/tmp
+
+    local gh_filename="gh_${GH_CLI_VERSION}_linux_amd64"
+    local gh_tar_file="${gh_filename}.tar.gz"
+
+    retry 5 curl -sL -o ${WORKSPACE}/tmp "https://github.com/cli/cli/releases/download/v${GH_CLI_VERSION}/${gh_tar_file}"
+
+    tar -C ${WORKSPACE}/bin -xpf ${WORKSPACE}/tmp/${gh_tar_file} ${gh_filename}/bin/gh --strip-components=2
+
+    chmod +x ${WORKSPACE}/bin/gh
+    rm -rf ${WORKSPACE}/tmp
+
+    gh version
+}
+
+with_jq() {
+    mkdir -p ${WORKSPACE}/bin
+    retry 5 curl -sL -o ${WORKSPACE}/bin/jq "https://github.com/stedolan/jq/releases/download/jq-${JQ_VERSION}/jq-linux64"
+
+    chmod +x ${WORKSPACE}/bin/jq
+    jq --version
+}
---
+++ .buildkite/scripts/test-with-integrations.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+
+set -euo pipefail
+
+WORKSPACE="$(pwd)"
+
+TMP_FOLDER_TEMPLATE="tmp.repo"
+
+cleanup() {
+    echo "Deleting temporal files..."
+    cd ${WORKSPACE}
+    rm -rf "${TMP_FOLDER_TEMPLATE}.*"
+    echo "Done."
+}
+
+trap cleanup EXIT
+source .buildkite/scripts/install_deps.sh
+
+add_bin_path
+
+with_github_cli
+with_jq
+
+INTEGRATIONS_SOURCE_BRANCH=main
+INTEGRATIONS_REPO=github.com:elastic/integrations.git
+INTEGRATIONS_PR_BRANCH="test-elastic-package-pr-${BUILDKITE_PULL_REQUEST}"
+INTEGRATIONS_PR_TITLE="Test elastic-package - DO NOT MERGE"
+
+
+get_pr_number() {
+    local branch="$1"
+    gh pr list -H "${branch}" --json number | jq -r '.[]|.number'
+}
+
+get_integrations_pr_link() {
+    local pr_number=$1
+    echo "https://github.com/elastic/integrations/pull/${pr_number}"
+}
+
+get_elastic_package_pr_link() {
+    echo "https://github.com/${GITHUB_PR_BASE_OWNER}/${GITHUB_PR_BASE_REPO}/pull/${BUILDKITE_PULL_REQUEST}"
+}
+
+set_git_config() {
+    git config user.name "${GITHUB_USERNAME_SECRET}"
+    git config user.email "${GITHUB_EMAIL_SECRET}"
+}
+
+git_push_with_auth() {
+    local branch=$1
+
+    retry 3 git push https://${GITHUB_USERNAME_SECRET}:${GITHUB_TOKEN}@github.com/${GITHUB_PR_BASE_OWNER}/${GITHUB_PR_BASE_REPO}.git "${branch}"
+}
+
+clone_repository() {
+    local target="$1"
+    retry 5 git clone https://github.com/elastic/integrations ${target}
+}
+
+create_pull_request() {
+    echo "Creating Pull Request"
+    retry 3 \
+        gh pr create \
+        --title "${INTEGRATIONS_PR_TITLE}" \
+        --body "Update elastic-package reference to ${GITHUB_PR_HEAD_SHA}.\nAutomated by [Buildkite build](${BUILDKITE_BUILD_URL})\n\nRelates: $(get_elastic_package_pr_link)" \
+        --draft \
+        --base ${INTEGRATIONS_SOURCE_BRANCH} \
+        --head ${INTEGRATIONS_PR_BRANCH} \
+        --assignee ${BUILDKITE_PR_HEAD_USER}
+}
+
+update_dependency() {
+    go mod edit -replace github.com/${GITHUB_PR_BASE_OWNER}/${GITHUB_PR_BASE_REPO}=github.com/${GITHUB_PR_OWNER}/${GITHUB_PR_REPO}@${GITHUB_PR_HEAD_SHA}
+    go mod tidy
+
+    git add go.mod
+    git add go.sum
+
+    git commit -m "Test elastic-package from PR ${BUILDKITE_PULL_REQUEST} - ${GITHUB_PR_HEAD_SHA}"
+}
+
+
+create_or_update_pull_request() {
+    local temp_path=$(mktemp -d -p ${WORKSPACE} -t ${TMP_FOLDER_TEMPLATE})
+    local repo_path="${temp_path}/elastic-integrations"
+    local checkout_options=""
+    local integrations_pr_number=""
+
+    clone_repository "${repo_path}"
+    pushd "${repo_path}" > /dev/null
+
+    set_git_config
+
+    integrations_pr_number=$(get_pr_number "${INTEGRATIONS_PR_BRANCH}")
+    if [ -z "${integrations_pr_number}" ]; then
+        checkout_options=" -b "
+    fi
+    git checkout ${checkout_options} ${INTEGRATIONS_PR_BRANCH}
+
+    update_dependency
+
+    return
+    git_push_with_auth
+
+    if [ -z "${integrations_pr_number}" ]; then
+        create_pull_request
+    fi
+
+    popd > /dev/null
+
+    rm -rf "${temp_path}"
+}
+
+
+add_pr_comment() {
+    local pr_number="$1"
+    retry 3 \
+        gh pr comment ${pr_number} \
+        --body "Created or updated PR in integrations repostiory to test this vesrion. Check ${get_integrations_pr_link}" \
+        --repo ${GITHUB_PR_BASE_OWNER}/${GITHUB_PR_BASE_REPO}
+}
+
+
+# integrations repository
+create_or_update_pull_request
+
+exit 0
+# elastic-package
+add_pr_comment "${BUILDKITE_PULL_REQUEST}"
