abfd2669d7792c99d58ef2a9e383e5e29bc2c984
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2023-05-30 16:48:55 +0200

    Add some tests for mapstr

---
+++ internal/common/mapstr_test.go
@@ -0,0 +1,60 @@
+package common
+
+import (
+	"encoding/json"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMapStrGetValue(t *testing.T) {
+
+	cases := []struct {
+		title         string
+		testFile      string
+		fieldKey      string
+		expectedValue interface{}
+	}{
+		{
+			title:         "string value",
+			testFile:      "./testdata/source.json",
+			fieldKey:      "host.architecture",
+			expectedValue: "x86_64",
+		},
+		{
+			title:         "float64 value",
+			testFile:      "./testdata/source.json",
+			fieldKey:      "metricset.period",
+			expectedValue: float64(10000),
+		},
+		{
+			title:         "slice value",
+			testFile:      "./testdata/source.json",
+			fieldKey:      "tags",
+			expectedValue: []interface{}{"apache_tomcat-cache", "forwarded"},
+		},
+		{
+			title:         "map value",
+			testFile:      "./testdata/source.json",
+			fieldKey:      "data_stream",
+			expectedValue: map[string]interface{}{"dataset": "apache_tomcat.cache", "namespace": "ep", "type": "metrics"},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.title, func(t *testing.T) {
+			b, err := os.ReadFile("./testdata/source.json")
+			require.NoError(t, err)
+
+			var given MapStr
+			err = json.Unmarshal(b, &given)
+			require.NoError(t, err)
+
+			val, err := given.GetValue(c.fieldKey)
+			assert.NoError(t, err)
+			assert.Equal(t, c.expectedValue, val)
+		})
+	}
+}
