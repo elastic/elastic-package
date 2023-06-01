de20b6d7605838b8641458616781f8c3857b996e
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2023-06-01 17:38:04 +0200

    Update pipeline to test with integrations (#1277)
    Update Buildkite pipeline to add all the required steps to create a
    Pull Request or update it to test the current elastic-package code
    with all the (integration and input) packages in the integrations
    repository

--- .buildkite/scripts/install_deps.sh
+++ .buildkite/scripts/install_deps.sh
@@ -39,3 +39,30 @@ with_docker_compose() {
     chmod +x ${WORKSPACE}/bin/docker-compose
     docker-compose version
 }
+
+with_github_cli() {
+    mkdir -p ${WORKSPACE}/bin
+    mkdir -p ${WORKSPACE}/tmp
+
+    local gh_filename="gh_${GH_CLI_VERSION}_linux_amd64"
+    local gh_tar_file="${gh_filename}.tar.gz"
+    local gh_tar_full_path="${WORKSPACE}/tmp/${gh_tar_file}"
+
+    retry 5 curl -sL -o ${gh_tar_full_path} "https://github.com/cli/cli/releases/download/v${GH_CLI_VERSION}/${gh_tar_file}"
+
+    # just extract the binary file from the tar.gz
+    tar -C ${WORKSPACE}/bin -xpf ${gh_tar_full_path} ${gh_filename}/bin/gh --strip-components=2
+
+    chmod +x ${WORKSPACE}/bin/gh
+    rm -rf ${WORKSPACE}/tmp
+
+    gh version
+}
+
+with_jq() {
+    mkdir -p ${WORKSPACE}/bin
+    retry 5 curl -sL -o ${WORKSPACE}/bin/jq "https://github.com/stedolan/jq/releases/download/jq-${JQ_VERSION}/jq-linux64"
+
+    chmod +x ${WORKSPACE}/bin/jq
+    jq --version
+}
