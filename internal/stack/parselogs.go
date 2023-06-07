b32a3e845cdf01c616bf564cb017209355ef6854
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2023-06-07 15:04:39 +0200

    Skipped malformed docker-compose log lines

--- internal/stack/parselogs.go
+++ internal/stack/parselogs.go
@@ -7,10 +7,11 @@ package stack
 import (
 	"bufio"
 	"encoding/json"
-	"fmt"
 	"os"
 	"strings"
 	"time"
+
+	"github.com/elastic/elastic-package/internal/logger"
 )
 
 type ParseLogsOptions struct {
@@ -41,7 +42,8 @@ func ParseLogs(options ParseLogsOptions, process func(log LogLine) error) error 
 		messageSlice := strings.SplitN(line, "|", 2)
 
 		if len(messageSlice) != 2 {
-			return fmt.Errorf("malformed docker-compose log line")
+			logger.Debugf("skipped malformed docker-compose log line: %s", line)
+			continue
 		}
 
 		messageLog := messageSlice[1]
