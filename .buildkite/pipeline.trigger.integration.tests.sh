aeaec982ca2f3d508833d2df38e2b253471bd19a
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2023-06-28 16:33:12 +0200

    Add prefix var

--- .buildkite/pipeline.trigger.integration.tests.sh
+++ .buildkite/pipeline.trigger.integration.tests.sh
@@ -9,6 +9,8 @@ echo "  - group: \":terminal: Integration test suite\""
 echo "    key: \"integration-tests\""
 echo "    steps:"
 
+CI_DEBUG_LOG_PREFIX=${CI_DEBUG_LOG_PREFIX:-elastic-package-debug-output}
+
 # Integration tests related stack command
 STACK_COMMAND_TESTS=(
     test-stack-command-default
@@ -28,7 +30,7 @@ for test in ${STACK_COMMAND_TESTS[@]}; do
     echo "          - build/elastic-stack-dump/stack/*/logs/fleet-server-internal/**/*"
     echo "          - build/elastic-stack-status/*/*"
     if [ "x${CI_DEBUG_LOG_FOLDER_PATH}" != "x" ]; then
-        echo "          - ${CI_DEBUG_LOG_FOLDER_PATH}/*"
+        echo "          - ${CI_DEBUG_LOG_FOLDER_PATH}/${CI_DEBUG_LOG_PREFIX}*.log"
     fi
 done
 
@@ -48,7 +50,7 @@ for test in ${CHECK_PACKAGES_TESTS[@]}; do
     echo "          - build/elastic-stack-dump/check-*/logs/*.log"
     echo "          - build/elastic-stack-dump/check-*/logs/fleet-server-internal/**/*"
     if [ "x${CI_DEBUG_LOG_FOLDER_PATH}" != "x" ]; then
-        echo "          - ${CI_DEBUG_LOG_FOLDER_PATH}/*"
+        echo "          - ${CI_DEBUG_LOG_FOLDER_PATH}/${CI_DEBUG_LOG_PREFIX}*.log"
     fi
     if [[ $test =~ with-kind$ ]]; then
         echo "          - build/kubectl-dump.txt"
@@ -68,7 +70,7 @@ for package in $(find . -maxdepth 1 -mindepth 1 -type d) ; do
     echo "        artifact_paths:"
     echo "          - build/test-results/*.xml"
     if [ "x${CI_DEBUG_LOG_FOLDER_PATH}" != "x" ]; then
-        echo "          - ${CI_DEBUG_LOG_FOLDER_PATH}/*"
+        echo "          - ${CI_DEBUG_LOG_FOLDER_PATH}/${CI_DEBUG_LOG_PREFIX}*.log"
     fi
 done
 
@@ -82,7 +84,7 @@ echo "        artifact_paths:"
 echo "          - build/elastic-stack-dump/build-zip/logs/*.log"
 echo "          - build/packages/*.sig"
 if [ "x${CI_DEBUG_LOG_FOLDER_PATH}" != "x" ]; then
-    echo "          - ${CI_DEBUG_LOG_FOLDER_PATH}/*"
+    echo "          - ${CI_DEBUG_LOG_FOLDER_PATH}/${CI_DEBUG_LOG_PREFIX}*.log"
 fi
 
 echo "      - label: \":go: Running integration test: test-install-zip\""
@@ -92,7 +94,7 @@ echo "          provider: \"gcp\""
 echo "        artifact_paths:"
 echo "          - build/elastic-stack-dump/install-zip/logs/*.log"
 if [ "x${CI_DEBUG_LOG_FOLDER_PATH}" != "x" ]; then
-    echo "          - ${CI_DEBUG_LOG_FOLDER_PATH}/*"
+    echo "          - ${CI_DEBUG_LOG_FOLDER_PATH}/${CI_DEBUG_LOG_PREFIX}*.log"
 fi
 
 echo "      - label: \":go: Running integration test: test-install-zip-shellinit\""
@@ -102,7 +104,7 @@ echo "          provider: \"gcp\""
 echo "        artifact_paths:"
 echo "          - build/elastic-stack-dump/install-zip-shellinit/logs/*.log"
 if [ "x${CI_DEBUG_LOG_FOLDER_PATH}" != "x" ]; then
-    echo "          - ${CI_DEBUG_LOG_FOLDER_PATH}/*"
+    echo "          - ${CI_DEBUG_LOG_FOLDER_PATH}/${CI_DEBUG_LOG_PREFIX}*.log"
 fi
 
 echo "      - label: \":go: Running integration test: test-profiles-command\""
@@ -111,5 +113,5 @@ echo "        agents:"
 echo "          provider: \"gcp\""
 if [ "x${CI_DEBUG_LOG_FOLDER_PATH}" != "x" ]; then
     echo "        artifact_paths:"
-    echo "          - ${CI_DEBUG_LOG_FOLDER_PATH}/*"
+    echo "          - ${CI_DEBUG_LOG_FOLDER_PATH}/${CI_DEBUG_LOG_PREFIX}*.log"
 fi
