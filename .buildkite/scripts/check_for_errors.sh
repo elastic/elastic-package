40b023d982d8922958d1fade2498796722d0bb87
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2023-06-29 10:25:58 +0200

    Show errors

--- .buildkite/scripts/check_for_errors.sh
+++ .buildkite/scripts/check_for_errors.sh
@@ -12,7 +12,12 @@ echo ----
 
 for package_type in $(ls build/output-logs/); do
     for output_file in $(ls build/output-logs/${package_type}); do
-        errors=$(grep -E "^Error:" build/output-logs/${package_type}/${output_file})
+        output="build/output-logs/${package_type}/${output_file}"
+        echo "Any error on ${output}?"
+        errors=$(grep -E "^Error:" ${output})
+
+        echo "Found errors":
+        echo "${errors}"
 
         if [ -n "${errors}" ]; then
             cat <<EOF >> markdown.md
@@ -20,7 +25,7 @@ for package_type in $(ls build/output-logs/); do
               > ${errors}
 EOF
         fi
-done
+    done
 done
 
 if [ -f markdown.md ]; then
