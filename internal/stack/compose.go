8a9596724648197abad7bb931362995f6a63c11a
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2023-07-04 13:08:42 +0200

    Clean docker-compose log from output (#1330)
    Remove progress information from docker-compose pull/up commands,
    being able to enable or disable it through an environment variable

--- internal/stack/compose.go
+++ internal/stack/compose.go
@@ -11,7 +11,6 @@ import (
 	"github.com/elastic/elastic-package/internal/compose"
 	"github.com/elastic/elastic-package/internal/docker"
 	"github.com/elastic/elastic-package/internal/install"
-	"github.com/elastic/elastic-package/internal/logger"
 )
 
 type ServiceStatus struct {
@@ -205,7 +204,6 @@ func dockerComposeStatus() ([]ServiceStatus, error) {
 		if err != nil {
 			return nil, err
 		}
-		logger.Debugf("Adding Service: \"%v\"", service.Name)
 		services = append(services, *service)
 	}
 
