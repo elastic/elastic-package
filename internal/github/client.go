2049d2b17c27dbd16eabcd2dc45f7cd967e3a442
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2023-08-29 16:44:40 +0200

    Use cache file

--- internal/github/client.go
+++ internal/github/client.go
@@ -5,12 +5,32 @@
 package github
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 
 	"github.com/google/go-github/v32/github"
 )
 
+type Client struct {
+	client *github.Client
+}
+
 // UnauthorizedClient function returns unauthorized instance of Github API client.
-func UnauthorizedClient() *github.Client {
-	return github.NewClient(new(http.Client))
+func UnauthorizedClient() *Client {
+	githubClient := github.NewClient(new(http.Client))
+	return &Client{githubClient}
+}
+
+func (c *Client) LatestRelease(ctx context.Context, repositoryOwner, repositoryName string) (*github.RepositoryRelease, error) {
+	release, _, err := c.client.Repositories.GetLatestRelease(ctx, repositoryOwner, repositoryName)
+	if err != nil {
+		return nil, fmt.Errorf("can't check latest release: %w", err)
+	}
+
+	if release.TagName == nil || *release.TagName == "" {
+		return nil, fmt.Errorf("release tag is empty")
+	}
+
+	return release, nil
 }
--- internal/version/check_update.go
+++ internal/version/check_update.go
@@ -6,11 +6,16 @@ package version
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/Masterminds/semver/v3"
 
+	"github.com/elastic/elastic-package/internal/configuration/locations"
 	"github.com/elastic/elastic-package/internal/environment"
 	"github.com/elastic/elastic-package/internal/github"
 	"github.com/elastic/elastic-package/internal/logger"
@@ -19,9 +24,19 @@ import (
 const (
 	repositoryOwner = "elastic"
 	repositoryName  = "elastic-package"
+
+	latestVersionFile    = "latestVersion"
+	defaultCacheDuration = "1m"
 )
 
 var checkUpdatedDisabledEnv = environment.WithElasticPackagePrefix("CHECK_UPDATE_DISABLED")
+
+type versionLatest struct {
+	// Release   gh.RepositoryRelease `json:"latest"`
+	TagName   string    `json:"tag"`
+	HtmlURL   string    `json:"html_url"`
+	Timestamp time.Time `json:"timestamp"`
+}
 
 // CheckUpdate function checks using Github Release API if newer version is available.
 func CheckUpdate() {
@@ -37,16 +52,30 @@ func CheckUpdate() {
 		return
 	}
 
-	githubClient := github.UnauthorizedClient()
-	release, _, err := githubClient.Repositories.GetLatestRelease(context.TODO(), repositoryOwner, repositoryName)
+	duration, err := time.ParseDuration(defaultCacheDuration)
 	if err != nil {
-		logger.Debugf("Error: can't check latest release, %v", err)
+		logger.Warnf("failed to parse cache duration: %s", err.Error())
 		return
 	}
-
-	if release.TagName == nil || *release.TagName == "" {
-		logger.Debugf("Error: release tag is empty")
-		return
+	latestVersion, expired, _ := checkCachedLatestVersion(duration)
+	var release *versionLatest
+	switch {
+	case !expired:
+		logger.Debugf("latest version (cached): %s", latestVersion)
+		release = latestVersion
+	default:
+		logger.Debugf("checking latest release in Github")
+		githubClient := github.UnauthorizedClient()
+		githubRelease, err := githubClient.LatestRelease(context.TODO(), repositoryOwner, repositoryName)
+		if err != nil {
+			logger.Debugf("Error: %v", err)
+			return
+		}
+		release = &versionLatest{
+			TagName:   *githubRelease.TagName,
+			HtmlURL:   *githubRelease.HTMLURL,
+			Timestamp: time.Now(),
+		}
 	}
 
 	currentVersion, err := semver.NewVersion(Tag[1:]) // strip "v" prefix
@@ -55,13 +84,64 @@ func CheckUpdate() {
 		return
 	}
 
-	releaseVersion, err := semver.NewVersion((*release.TagName)[1:]) // strip "v" prefix
+	releaseVersion, err := semver.NewVersion(release.TagName[1:]) // strip "v" prefix
 	if err != nil {
 		logger.Debugf("Error: can't parse current version tag, %v", err)
 		return
 	}
 
 	if currentVersion.LessThan(releaseVersion) {
-		logger.Infof("New version is available - %s. Download from: %s", *release.TagName, *release.HTMLURL)
+		logger.Infof("New version is available - %s. Download from: %s", release.TagName, release.HtmlURL)
 	}
+
+	if err := writeLatestReleaseToCache(release); err != nil {
+		logger.Debugf("failed to write latest versoin to cache file: %v", err)
+	}
+}
+
+func writeLatestReleaseToCache(release *versionLatest) error {
+	elasticPackagePath, err := locations.NewLocationManager()
+	if err != nil {
+		return fmt.Errorf("failed locating the configuration directory: %w", err)
+	}
+
+	latestVersionPath := filepath.Join(elasticPackagePath.RootDir(), latestVersionFile)
+
+	contents, err := json.Marshal(release)
+	if err != nil {
+		return fmt.Errorf("failed to encode file %s: %w", latestVersionPath, err)
+	}
+	err = os.WriteFile(latestVersionPath, contents, 0644)
+	if err != nil {
+		return fmt.Errorf("writing file failed (path: %s): %w", latestVersionPath, err)
+	}
+
+	return nil
+}
+
+func checkCachedLatestVersion(expiration time.Duration) (*versionLatest, bool, error) {
+	elasticPackagePath, err := locations.NewLocationManager()
+	if err != nil {
+		return nil, true, fmt.Errorf("failed locating the configuration directory: %w", err)
+	}
+
+	latestVersionPath := filepath.Join(elasticPackagePath.RootDir(), latestVersionFile)
+	contents, err := os.ReadFile(latestVersionPath)
+	if err != nil {
+		logger.Warnf("reading version file failed: %w", err.Error())
+		return nil, true, fmt.Errorf("reading version file failed: %w", err)
+	}
+
+	var infoVersion versionLatest
+	err = json.Unmarshal(contents, &infoVersion)
+	if err != nil {
+		return nil, true, fmt.Errorf("failed to decode file %s: %w", latestVersionPath, err)
+	}
+
+	exprirationTime := time.Now().Add(-expiration)
+
+	if infoVersion.Timestamp.Before(exprirationTime) {
+		return nil, true, fmt.Errorf("expired version: %s (%s)", infoVersion.TagName, infoVersion.Timestamp)
+	}
+	return &infoVersion, false, nil
 }
