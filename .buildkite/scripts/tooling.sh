950abd3e7a8814bf71bbf79bf96887e798b488dd
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2023-10-19 12:18:49 +0200

    Review buildkite scripts cleanup process (#1487)
    Review buildkite scripts and hooks to ensure that the cleanup
    process performs all the needed operations

--- .buildkite/scripts/tooling.sh
+++ .buildkite/scripts/tooling.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -euo pipefail
 
+unset_secrets () {
+  for var in $(printenv | sed 's;=.*;;' | sort); do
+    if [[ "$var" == *_SECRET || "$var" == *_TOKEN ]]; then
+        unset "$var"
+    fi
+  done
+}
+
 repo_name() {
     # Example of URL: git@github.com:acme-inc/my-project.git
     local repoUrl=$1
@@ -43,4 +51,18 @@ retry() {
         fi
     done
     return 0
+}
+
+google_cloud_logout_active_account() {
+  local active_account=$(gcloud auth list --filter=status:ACTIVE --format="value(account)" 2>/dev/null)
+  if [[ -n "$active_account" && -n "${GOOGLE_APPLICATION_CREDENTIALS+x}" ]]; then
+    echo "Logging out from GCP for active account"
+    gcloud auth revoke $active_account > /dev/null 2>&1
+  else
+    echo "No active GCP accounts found."
+  fi
+  if [ -n "${GOOGLE_APPLICATION_CREDENTIALS+x}" ]; then
+    rm -rf ${GOOGLE_APPLICATION_CREDENTIALS}
+    unset GOOGLE_APPLICATION_CREDENTIALS
+  fi
 }
