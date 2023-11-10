4365fec4d2f06edcefd746044c4bdda3f10b0cf0
Andrea Spacca <andrea.spacca@elastic.co>
2023-11-10 09:24:26 +0900

    minor fixes in benhcmark rally

--- internal/benchrunner/runners/rally/runner.go
+++ internal/benchrunner/runners/rally/runner.go
@@ -16,13 +16,13 @@ import (
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"text/template"
 	"time"
 
 	"github.com/elastic/elastic-package/internal/packages/installer"
 
 	"github.com/magefile/mage/sh"
 
-	"github.com/elastic/elastic-package/internal/corpusgenerator"
 	"github.com/elastic/elastic-package/internal/stack"
 
 	"github.com/google/uuid"
@@ -50,6 +50,42 @@ const (
 
 	// BenchType defining rally benchmark
 	BenchType benchrunner.Type = "rally"
+
+	rallyTrackTemplate = `{% import "rally.helpers" as rally with context %}
+{
+  "version": 2,
+  "description": "Track for [[.DataStream]]",
+  "datastream": [
+    {
+      "name": "[[.DataStream]]",
+      "body": "[[.CorpusFilename]]"
+    }
+  ],
+  "corpora": [
+    {
+      "name": "[[.CorpusFilename]]",
+      "documents": [
+        {
+          "target-data-stream": "[[.DataStream]]",
+          "source-file": "[[.CorpusFilename]]",
+          "document-count": [[.CorpusDocsCount]],
+          "uncompressed-bytes": [[.CorpusSizeInBytes]]
+        }
+      ]
+    }
+  ],
+  "schedule": [
+    {
+      "operation": {
+        "operation-type": "bulk",
+        "bulk-size": {{bulk_size | default(5000)}},
+        "ingest-percentage": {{ingest_percentage | default(100)}}
+      },
+      "clients": {{bulk_indexing_clients | default(8)}}
+    }
+  ]
+}
+`
 )
 
 var ErrDryRun = errors.New("dry run: rally benchmark not executed")
@@ -513,7 +549,7 @@ func (r *runner) runGenerator(destDir string) error {
 		return fmt.Errorf("cannot not create rally track file: %w", err)
 	}
 	r.trackFile = trackFile.Name()
-	rallyTrackContent, err := corpusgenerator.GenerateRallyTrack(r.runtimeDataStream, corpusFile, corpusDocsCount)
+	rallyTrackContent, err := generateRallyTrack(r.runtimeDataStream, corpusFile, corpusDocsCount)
 	if err != nil {
 		return fmt.Errorf("cannot not generate rally track content: %w", err)
 	}
@@ -870,4 +906,35 @@ func createRunID() string {
 
 func getDataStreamPath(packageRoot, dataStream string) string {
 	return filepath.Join(packageRoot, "data_stream", dataStream)
+}
+
+func generateRallyTrack(dataStream string, corpusFile *os.File, corpusDocsCount uint64) ([]byte, error) {
+	t := template.New("rallytrack")
+
+	parsedTpl, err := t.Delims("[[", "]]").Parse(rallyTrackTemplate)
+	if err != nil {
+		return nil, fmt.Errorf("error while parsing rally track template: %w", err)
+	}
+
+	fi, err := corpusFile.Stat()
+	if err != nil {
+		return nil, fmt.Errorf("error with stat on rally corpus file: %w", err)
+	}
+
+	corpusSizeInBytes := fi.Size()
+
+	buf := new(bytes.Buffer)
+	templateData := map[string]any{
+		"DataStream":        dataStream,
+		"CorpusFilename":    filepath.Base(corpusFile.Name()),
+		"CorpusDocsCount":   corpusDocsCount,
+		"CorpusSizeInBytes": corpusSizeInBytes,
+	}
+
+	err = parsedTpl.Execute(buf, templateData)
+	if err != nil {
+		return nil, fmt.Errorf("error on parsin on rally track template: %w", err)
+	}
+
+	return buf.Bytes(), nil
 }
