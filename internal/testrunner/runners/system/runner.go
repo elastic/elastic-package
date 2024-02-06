d87708916ea9cf2a2bec6c3718e42c84f0d3df38
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2024-02-06 18:35:15 +0100

    Set debug level in system tests for agent

--- internal/testrunner/runners/system/runner.go
+++ internal/testrunner/runners/system/runner.go
@@ -102,11 +102,12 @@ type runner struct {
 	options   testrunner.TestOptions
 	pipelines []ingest.Pipeline
 	// Execution order of following handlers is defined in runner.TearDown() method.
-	deleteTestPolicyHandler func() error
-	deletePackageHandler    func() error
-	resetAgentPolicyHandler func() error
-	shutdownServiceHandler  func() error
-	wipeDataStreamHandler   func() error
+	deleteTestPolicyHandler   func() error
+	deletePackageHandler      func() error
+	resetAgentPolicyHandler   func() error
+	resetAgentLogLevelHandler func() error
+	shutdownServiceHandler    func() error
+	wipeDataStreamHandler     func() error
 }
 
 // Type returns the type of test that can be run by this test runner.
@@ -151,6 +152,13 @@ func (r *runner) tearDownTest() error {
 			return err
 		}
 		r.resetAgentPolicyHandler = nil
+	}
+
+	if r.resetAgentLogLevelHandler != nil {
+		if err := r.resetAgentLogLevelHandler(); err != nil {
+			return err
+		}
+		r.resetAgentLogLevelHandler = nil
 	}
 
 	if r.deleteTestPolicyHandler != nil {
@@ -630,6 +638,21 @@ func (r *runner) runTest(config *testConfig, ctxt servicedeployer.ServiceContext
 	origPolicy := kibana.Policy{
 		ID:       agent.PolicyID,
 		Revision: agent.PolicyRevision,
+	}
+
+	logger.Debug("Set Debug log level to agent")
+	origLogLevel := agent.LocalMetadata.Elastic.Agent.LogLevel
+	err = r.options.KibanaClient.SetLogLevelAgent(agent.ID, "debug")
+	if err != nil {
+		return result.WithError(fmt.Errorf("error setting log level debug for agent %s: %w", agent.ID, err))
+	}
+	r.resetAgentLogLevelHandler = func() error {
+		logger.Debugf("reassigning original log level %q back to agent...", origLogLevel)
+
+		if err := r.options.KibanaClient.SetLogLevelAgent(agent.ID, origLogLevel); err != nil {
+			return fmt.Errorf("error reassigning original log level to agent: %w", err)
+		}
+		return nil
 	}
 
 	// Assign policy to agent
