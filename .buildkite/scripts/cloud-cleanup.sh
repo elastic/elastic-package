85e69c207f47f0dc968cc88a91ff8bf7b8f15403
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2024-02-21 19:26:36 +0100

    Add pipeline to cleanup cloud resources

---
+++ .buildkite/scripts/cloud-cleanup.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+export CREATION_DATE=$(date -Is -d "24 hours ago")
+
+echo "--- Cleaning up AWS resources..."
+docker run -v $(pwd)/.buildkite/misc/gce-cleanup.yml:/etc/cloud-reaper/config.yml \
+  -e ACCOUNT_SECRET="${ELASTIC_PACKAGE_AWS_SECRET_KEY}" \
+  -e ACCOUNT_KEY="${ELASTIC_PACKAGE_AWS_ACCESS_KEY}" \
+  -e ACCOUNT_PROJECT="${ELASTIC_PACKAGE_AWS_USER_SECRET}"
+  -e CREATION_DATE=${CREATION_DATE} \
+  ${DOCKER_REGISTRY}/observability-ci/cloud-reaper:0.3.0 cloud-reaper \
+    --config /etc/cloud-reaper/config.yml \
+    plan
+
+echo "--- Cleaning up GCP resources..."
+docker run -v $(pwd)/.buildkite/misc/gce-cleanup.yml:/etc/cloud-reaper/config.yml \
+  -e ACCOUNT_SECRET="${ELASTIC_PACKAGE_GCP_KEY_SECRET}" \
+  -e ACCOUNT_KEY="${ELASTIC_PACKAGE_GCP_EMAIL_SECRET}" \
+  -e ACCOUNT_PROJECT="${ELASTIC_PACKAGE_GCP_PROJECT_SECRET}"
+  -e CREATION_DATE=${CREATION_DATE} \
+  ${DOCKER_REGISTRY}/observability-ci/cloud-reaper:0.3.0 cloud-reaper \
+    --config /etc/cloud-reaper/config.yml \
+    plan
