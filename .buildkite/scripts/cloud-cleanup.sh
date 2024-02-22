890a985e5f9c6c8fd2d02ae488b5181625b8046b
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2024-02-22 17:46:47 +0100

    Set quiet for unzip

--- .buildkite/scripts/cloud-cleanup.sh
+++ .buildkite/scripts/cloud-cleanup.sh
@@ -29,7 +29,7 @@ echo "--- Cleaning up other AWS resources"
 echo "--- Installing awscli"
 if ! which aws; then
   curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-  unzip awscliv2.zip
+  unzip -q awscliv2.zip
   sudo ./aws/install
   rm -rf awscliv2.zip aws
   aws --version
