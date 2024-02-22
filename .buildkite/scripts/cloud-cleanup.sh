214a2c719c1d619f00f8d5048e9229dd80ee3948
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2024-02-22 18:02:17 +0100

    Add validation commands

--- .buildkite/scripts/cloud-cleanup.sh
+++ .buildkite/scripts/cloud-cleanup.sh
@@ -6,6 +6,17 @@ set -euo pipefail
 export CREATION_DATE=$(date -Is -d "1 minute ago")
 
 echo "--- Cleaning up GCP resources..."
+echo "Validating configuration"
+docker run -v $(pwd)/.buildkite/configs/cleanup.gcp.yml:/etc/cloud-reaper/config.yml \
+  -e ACCOUNT_SECRET="${ELASTIC_PACKAGE_GCP_KEY_SECRET}" \
+  -e ACCOUNT_KEY="${ELASTIC_PACKAGE_GCP_EMAIL_SECRET}" \
+  -e ACCOUNT_PROJECT="${ELASTIC_PACKAGE_GCP_PROJECT_SECRET}" \
+  -e CREATION_DATE=${CREATION_DATE} \
+  ${DOCKER_REGISTRY}/observability-ci/cloud-reaper:0.3.0 cloud-reaper \
+    --config /etc/cloud-reaper/config.yml \
+    validate
+
+echo "Scanning resources"
 docker run -v $(pwd)/.buildkite/configs/cleanup.gcp.yml:/etc/cloud-reaper/config.yml \
   -e ACCOUNT_SECRET="${ELASTIC_PACKAGE_GCP_KEY_SECRET}" \
   -e ACCOUNT_KEY="${ELASTIC_PACKAGE_GCP_EMAIL_SECRET}" \
@@ -16,6 +27,17 @@ docker run -v $(pwd)/.buildkite/configs/cleanup.gcp.yml:/etc/cloud-reaper/config
     plan
 
 echo "--- Cleaning up AWS resources..."
+echo "Validating configuration"
+docker run -v $(pwd)/.buildkite/configs/cleanup.aws.yml:/etc/cloud-reaper/config.yml \
+  -e ACCOUNT_SECRET="${ELASTIC_PACKAGE_AWS_SECRET_KEY}" \
+  -e ACCOUNT_KEY="${ELASTIC_PACKAGE_AWS_ACCESS_KEY}" \
+  -e ACCOUNT_PROJECT="${ELASTIC_PACKAGE_AWS_USER_SECRET}" \
+  -e CREATION_DATE=${CREATION_DATE} \
+  ${DOCKER_REGISTRY}/observability-ci/cloud-reaper:0.3.0 cloud-reaper \
+    --config /etc/cloud-reaper/config.yml \
+    validate
+
+echo "Scanning resources"
 docker run -v $(pwd)/.buildkite/configs/cleanup.aws.yml:/etc/cloud-reaper/config.yml \
   -e ACCOUNT_SECRET="${ELASTIC_PACKAGE_AWS_SECRET_KEY}" \
   -e ACCOUNT_KEY="${ELASTIC_PACKAGE_AWS_ACCESS_KEY}" \
@@ -28,7 +50,7 @@ docker run -v $(pwd)/.buildkite/configs/cleanup.aws.yml:/etc/cloud-reaper/config
 echo "--- Cleaning up other AWS resources"
 echo "--- Installing awscli"
 if ! which aws; then
-  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+  curl -s "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
   unzip -q awscliv2.zip
   sudo ./aws/install
   rm -rf awscliv2.zip aws
