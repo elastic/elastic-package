92879a33ba1b4e71345570ee2097b30e1bd5e698
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2024-02-23 12:43:01 +0100

    Refactor functions and rename variable for date

--- .buildkite/scripts/cloud-cleanup.sh
+++ .buildkite/scripts/cloud-cleanup.sh
@@ -8,7 +8,7 @@ AWS_RESOURCES_FILE="aws.resources.txt"
 GCP_RESOURCES_FILE="gcp.resources.txt"
 
 RESOURCE_RETENTION_PERIOD="${RESOURCE_RETENTION_PERIOD:-"24 hours"}"
-export CREATION_DATE=$(date -Is -d "${RESOURCE_RETENTION_PERIOD} ago")
+export DELETE_RESOURCES_BEFORE_DATE=$(date -Is -d "${RESOURCE_RETENTION_PERIOD} ago")
 
 resource_to_delete=0
 
@@ -22,51 +22,59 @@ any_resources_to_delete() {
     return 0
 }
 
-echo "--- Cleaning up GCP resources..."
-echo "Validating configuration"
-docker run -v $(pwd)/.buildkite/configs/cleanup.gcp.yml:/etc/cloud-reaper/config.yml \
-  -e ACCOUNT_SECRET="${ELASTIC_PACKAGE_GCP_KEY_SECRET}" \
-  -e ACCOUNT_KEY="${ELASTIC_PACKAGE_GCP_EMAIL_SECRET}" \
-  -e ACCOUNT_PROJECT="${ELASTIC_PACKAGE_GCP_PROJECT_SECRET}" \
-  -e CREATION_DATE=${CREATION_DATE} \
-  ${DOCKER_REGISTRY}/observability-ci/cloud-reaper:0.3.0 cloud-reaper \
-    --config /etc/cloud-reaper/config.yml \
-    validate
+cloud_reaper_aws() {
+    echo "Validating configuration"
+    docker run -v $(pwd)/.buildkite/configs/cleanup.aws.yml:/etc/cloud-reaper/config.yml \
+      -e ACCOUNT_SECRET="${ELASTIC_PACKAGE_AWS_SECRET_KEY}" \
+      -e ACCOUNT_KEY="${ELASTIC_PACKAGE_AWS_ACCESS_KEY}" \
+      -e ACCOUNT_PROJECT="${ELASTIC_PACKAGE_AWS_USER_SECRET}" \
+      -e CREATION_DATE="${DELETE_RESOURCES_BEFORE_DATE}" \
+      ${DOCKER_REGISTRY}/observability-ci/cloud-reaper:0.3.0 cloud-reaper \
+        --config /etc/cloud-reaper/config.yml \
+        validate
 
-echo "Scanning resources"
-docker run -v $(pwd)/.buildkite/configs/cleanup.gcp.yml:/etc/cloud-reaper/config.yml \
-  -e ACCOUNT_SECRET="${ELASTIC_PACKAGE_GCP_KEY_SECRET}" \
-  -e ACCOUNT_KEY="${ELASTIC_PACKAGE_GCP_EMAIL_SECRET}" \
-  -e ACCOUNT_PROJECT="${ELASTIC_PACKAGE_GCP_PROJECT_SECRET}" \
-  -e CREATION_DATE=${CREATION_DATE} \
-  ${DOCKER_REGISTRY}/observability-ci/cloud-reaper:0.3.0 cloud-reaper \
-    --config /etc/cloud-reaper/config.yml \
-    plan | tee "${GCP_RESOURCES_FILE}"
+    echo "Scanning resources"
+    docker run -v $(pwd)/.buildkite/configs/cleanup.aws.yml:/etc/cloud-reaper/config.yml \
+      -e ACCOUNT_SECRET="${ELASTIC_PACKAGE_AWS_SECRET_KEY}" \
+      -e ACCOUNT_KEY="${ELASTIC_PACKAGE_AWS_ACCESS_KEY}" \
+      -e ACCOUNT_PROJECT="${ELASTIC_PACKAGE_AWS_USER_SECRET}" \
+      -e CREATION_DATE="${DELETE_RESOURCES_BEFORE_DATE}" \
+      ${DOCKER_REGISTRY}/observability-ci/cloud-reaper:0.3.0 cloud-reaper \
+        --config /etc/cloud-reaper/config.yml \
+        plan | tee "${AWS_RESOURCES_FILE}"
+}
+
+cloud_reaper_gcp() {
+    echo "Validating configuration"
+    docker run -v $(pwd)/.buildkite/configs/cleanup.gcp.yml:/etc/cloud-reaper/config.yml \
+      -e ACCOUNT_SECRET="${ELASTIC_PACKAGE_GCP_KEY_SECRET}" \
+      -e ACCOUNT_KEY="${ELASTIC_PACKAGE_GCP_EMAIL_SECRET}" \
+      -e ACCOUNT_PROJECT="${ELASTIC_PACKAGE_GCP_PROJECT_SECRET}" \
+      -e CREATION_DATE="${DELETE_RESOURCES_BEFORE_DATE}" \
+      ${DOCKER_REGISTRY}/observability-ci/cloud-reaper:0.3.0 cloud-reaper \
+        --config /etc/cloud-reaper/config.yml \
+        validate
+
+    echo "Scanning resources"
+    docker run -v $(pwd)/.buildkite/configs/cleanup.gcp.yml:/etc/cloud-reaper/config.yml \
+      -e ACCOUNT_SECRET="${ELASTIC_PACKAGE_GCP_KEY_SECRET}" \
+      -e ACCOUNT_KEY="${ELASTIC_PACKAGE_GCP_EMAIL_SECRET}" \
+      -e ACCOUNT_PROJECT="${ELASTIC_PACKAGE_GCP_PROJECT_SECRET}" \
+      -e CREATION_DATE="${DELETE_RESOURCES_BEFORE_DATE}" \
+      ${DOCKER_REGISTRY}/observability-ci/cloud-reaper:0.3.0 cloud-reaper \
+        --config /etc/cloud-reaper/config.yml \
+        plan | tee "${GCP_RESOURCES_FILE}"
+}
+
+echo "--- Cleaning up GCP resources..."
+cloud_reaper_gcp
 
 if any_resources_to_delete "${GCP_RESOURCES_FILE}"; then
     resources_to_delete=1
 fi
 
 echo "--- Cleaning up AWS resources..."
-echo "Validating configuration"
-docker run -v $(pwd)/.buildkite/configs/cleanup.aws.yml:/etc/cloud-reaper/config.yml \
-  -e ACCOUNT_SECRET="${ELASTIC_PACKAGE_AWS_SECRET_KEY}" \
-  -e ACCOUNT_KEY="${ELASTIC_PACKAGE_AWS_ACCESS_KEY}" \
-  -e ACCOUNT_PROJECT="${ELASTIC_PACKAGE_AWS_USER_SECRET}" \
-  -e CREATION_DATE=${CREATION_DATE} \
-  ${DOCKER_REGISTRY}/observability-ci/cloud-reaper:0.3.0 cloud-reaper \
-    --config /etc/cloud-reaper/config.yml \
-    validate
-
-echo "Scanning resources"
-docker run -v $(pwd)/.buildkite/configs/cleanup.aws.yml:/etc/cloud-reaper/config.yml \
-  -e ACCOUNT_SECRET="${ELASTIC_PACKAGE_AWS_SECRET_KEY}" \
-  -e ACCOUNT_KEY="${ELASTIC_PACKAGE_AWS_ACCESS_KEY}" \
-  -e ACCOUNT_PROJECT="${ELASTIC_PACKAGE_AWS_USER_SECRET}" \
-  -e CREATION_DATE=${CREATION_DATE} \
-  ${DOCKER_REGISTRY}/observability-ci/cloud-reaper:0.3.0 cloud-reaper \
-    --config /etc/cloud-reaper/config.yml \
-    plan | tee "${AWS_RESOURCES_FILE}"
+cloud_reaper_aws
 
 if ! any_resources_to_delete "${AWS_RESOURCES_FILE}" ; then
     resource_to_delete=1
@@ -92,7 +100,7 @@ export AWS_ACCESS_KEY_ID="${ELASTIC_PACKAGE_AWS_ACCESS_KEY}"
 export AWS_SECRET_ACCESS_KEY="${ELASTIC_PACKAGE_AWS_ACCESS_KEY}"
 export AWS_DEFAULT_REGION=us-east-1
 
-echo "+++ Cleaning up Redshift resources"
+echo "+++ Cleaning up Redshift clusters"
 echo "+++ Cleaning up IAM roles"
 echo "+++ Cleaning up IAM policies"
 echo "+++ Cleaning up Schedulers"
