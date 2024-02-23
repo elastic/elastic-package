13ad1c51ec9ecaecaebf6d118d20019728d1fc8a
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2024-02-23 15:36:08 +0100

    Add dry-run option

--- .buildkite/scripts/cloud-cleanup.sh
+++ .buildkite/scripts/cloud-cleanup.sh
@@ -10,7 +10,16 @@ GCP_RESOURCES_FILE="gcp.resources.txt"
 RESOURCE_RETENTION_PERIOD="${RESOURCE_RETENTION_PERIOD:-"24 hours"}"
 export DELETE_RESOURCES_BEFORE_DATE=$(date -Is -d "${RESOURCE_RETENTION_PERIOD} ago")
 
+CLOUD_REAPER_IMAGE="${DOCKER_REGISTRY}/observability-ci/cloud-reaper:0.3.0 cloud-reaper"
+
 resource_to_delete=0
+
+COMMAND="validate"
+if [[ "${DRY_RUN}" != "true" ]]; then
+    COMMAND="plan" # TODO: to be changed to "destroy --confirm"
+else
+    COMMAND="plan"
+fi
 
 any_resources_to_delete() {
     local file=$1
@@ -24,56 +33,60 @@ any_resources_to_delete() {
 
 cloud_reaper_aws() {
     echo "Validating configuration"
-    docker run -v $(pwd)/.buildkite/configs/cleanup.aws.yml:/etc/cloud-reaper/config.yml \
+    docker run --rm -v $(pwd)/.buildkite/configs/cleanup.aws.yml:/etc/cloud-reaper/config.yml \
       -e ACCOUNT_SECRET="${ELASTIC_PACKAGE_AWS_SECRET_KEY}" \
       -e ACCOUNT_KEY="${ELASTIC_PACKAGE_AWS_ACCESS_KEY}" \
       -e ACCOUNT_PROJECT="${ELASTIC_PACKAGE_AWS_USER_SECRET}" \
       -e CREATION_DATE="${DELETE_RESOURCES_BEFORE_DATE}" \
-      ${DOCKER_REGISTRY}/observability-ci/cloud-reaper:0.3.0 cloud-reaper \
-        --config /etc/cloud-reaper/config.yml \
-        validate
+      "${CLOUD_REAPER_IMAGE}" \
+        cloud-reaper \
+          --config /etc/cloud-reaper/config.yml \
+          validate
 
     echo "Scanning resources"
-    docker run -v $(pwd)/.buildkite/configs/cleanup.aws.yml:/etc/cloud-reaper/config.yml \
+    docker run --rm -v $(pwd)/.buildkite/configs/cleanup.aws.yml:/etc/cloud-reaper/config.yml \
       -e ACCOUNT_SECRET="${ELASTIC_PACKAGE_AWS_SECRET_KEY}" \
       -e ACCOUNT_KEY="${ELASTIC_PACKAGE_AWS_ACCESS_KEY}" \
       -e ACCOUNT_PROJECT="${ELASTIC_PACKAGE_AWS_USER_SECRET}" \
       -e CREATION_DATE="${DELETE_RESOURCES_BEFORE_DATE}" \
-      ${DOCKER_REGISTRY}/observability-ci/cloud-reaper:0.3.0 cloud-reaper \
-        --config /etc/cloud-reaper/config.yml \
-        plan | tee "${AWS_RESOURCES_FILE}"
+      "${CLOUD_REAPER_IMAGE}" \
+        cloud-reaper \
+          --config /etc/cloud-reaper/config.yml \
+          ${COMMAND} | tee "${AWS_RESOURCES_FILE}"
 }
 
 cloud_reaper_gcp() {
     echo "Validating configuration"
-    docker run -v $(pwd)/.buildkite/configs/cleanup.gcp.yml:/etc/cloud-reaper/config.yml \
+    docker run --rm -v $(pwd)/.buildkite/configs/cleanup.gcp.yml:/etc/cloud-reaper/config.yml \
       -e ACCOUNT_SECRET="${ELASTIC_PACKAGE_GCP_KEY_SECRET}" \
       -e ACCOUNT_KEY="${ELASTIC_PACKAGE_GCP_EMAIL_SECRET}" \
       -e ACCOUNT_PROJECT="${ELASTIC_PACKAGE_GCP_PROJECT_SECRET}" \
       -e CREATION_DATE="${DELETE_RESOURCES_BEFORE_DATE}" \
-      ${DOCKER_REGISTRY}/observability-ci/cloud-reaper:0.3.0 cloud-reaper \
-        --config /etc/cloud-reaper/config.yml \
-        validate
+      "${CLOUD_REAPER_IMAGE}" \
+        cloud-reaper \
+          --config /etc/cloud-reaper/config.yml \
+          validate
 
     echo "Scanning resources"
-    docker run -v $(pwd)/.buildkite/configs/cleanup.gcp.yml:/etc/cloud-reaper/config.yml \
+    docker run --rm -v $(pwd)/.buildkite/configs/cleanup.gcp.yml:/etc/cloud-reaper/config.yml \
       -e ACCOUNT_SECRET="${ELASTIC_PACKAGE_GCP_KEY_SECRET}" \
       -e ACCOUNT_KEY="${ELASTIC_PACKAGE_GCP_EMAIL_SECRET}" \
       -e ACCOUNT_PROJECT="${ELASTIC_PACKAGE_GCP_PROJECT_SECRET}" \
       -e CREATION_DATE="${DELETE_RESOURCES_BEFORE_DATE}" \
-      ${DOCKER_REGISTRY}/observability-ci/cloud-reaper:0.3.0 cloud-reaper \
-        --config /etc/cloud-reaper/config.yml \
-        plan | tee "${GCP_RESOURCES_FILE}"
+      "${CLOUD_REAPER_IMAGE}" \
+        cloud-reaper \
+          --config /etc/cloud-reaper/config.yml \
+          ${COMMAND} | tee "${GCP_RESOURCES_FILE}"
 }
 
-echo "--- Cleaning up GCP resources..."
+echo "--- Cleaning up GCP resources older than ${DELETE_RESOURCES_BEFORE_DATE}..."
 cloud_reaper_gcp
 
 if any_resources_to_delete "${GCP_RESOURCES_FILE}"; then
     resources_to_delete=1
 fi
 
-echo "--- Cleaning up AWS resources..."
+echo "--- Cleaning up AWS resources older than ${DELETE_RESOURCES_BEFORE_DATE}..."
 cloud_reaper_aws
 
 if ! any_resources_to_delete "${AWS_RESOURCES_FILE}" ; then
@@ -82,17 +95,18 @@ fi
 
 if [ "${resource_to_delete}" -eq 1 ]; then
     message="There are resources to be deleted"
+    echo "${message}"
     if running_on_buildkite ; then
          buildkite-agent annotate \
-             "There are resources to be deleted" \
+             "${message}" \
+             --context "ctx-cloud-reaper-error" \
              --style "error"
     fi
-    echo "There are resources to be deleted"
     exit 1
 fi
 
 # TODO: List and delete the required resources using aws cli
-echo "--- Cleaning up other AWS resources"
+echo "--- Cleaning up other AWS resources older than ${DELETE_RESOURCES_BEFORE_DATE}"
 echo "--- Installing awscli"
 with_aws_cli
 
