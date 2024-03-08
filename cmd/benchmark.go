68679c2b45c6afe2d20ec1ef7bcac2933a9582ad
Joe Reuter <johannes.reuter@elastic.co>
2024-03-08 17:06:22 +0100

    remove unncessary stuff@

--- cmd/benchmark.go
+++ cmd/benchmark.go
@@ -531,14 +531,6 @@ func getValidateCommand() *cobra.Command {
 		RunE:  validateCommandAction,
 	}
 
-	// cmd.Flags().StringP(cobraext.BenchNameFlagName, "", "", cobraext.BenchNameFlagDescription)
-	// cmd.Flags().String(cobraext.VariantFlagName, "", cobraext.VariantFlagDescription)
-	// cmd.Flags().DurationP(cobraext.BenchStreamBackFillFlagName, "", 15*time.Minute, cobraext.BenchStreamBackFillFlagDescription)
-	// cmd.Flags().Uint64P(cobraext.BenchStreamEventsPerPeriodFlagName, "", 10, cobraext.BenchStreamEventsPerPeriodFlagDescription)
-	// cmd.Flags().DurationP(cobraext.BenchStreamPeriodDurationFlagName, "", 10*time.Second, cobraext.BenchStreamPeriodDurationFlagDescription)
-	// cmd.Flags().BoolP(cobraext.BenchStreamPerformCleanupFlagName, "", false, cobraext.BenchStreamPerformCleanupFlagDescription)
-	// cmd.Flags().StringP(cobraext.BenchStreamTimestampFieldFlagName, "", "timestamp", cobraext.BenchStreamTimestampFieldFlagDescription)
-
 	return cmd
 }
 
