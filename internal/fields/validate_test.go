151c5270ece5d8881c5adc1e12014b7a3db4f4a3
Maurizio Branca <maurizio.branca@gmail.com>
2024-03-08 18:44:26 +0100

    Add upcoming 7.17.x patch releases
    Adding a few extra releases to support unlikely yet possible edge
    cases.

--- internal/fields/validate_test.go
+++ internal/fields/validate_test.go
@@ -970,6 +970,7 @@ func TestValidateStackVersionsWithEcsMappings(t *testing.T) {
 		SupportEcs  bool
 	}{
 		{"^7.17.0", false},
+		{"7.17.19 || > 8.13", false},
 		{"^7.14.0 || ^8.0.0", false},
 		{"^7.14.1 || ^8.0.0", false},
 		{"^7.14.1 || ^8.8.0", false},
