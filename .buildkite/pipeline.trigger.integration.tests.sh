90cd7909965dd51c2042920513d8070f7d80e26c
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2024-03-25 13:34:11 +0100

    Test with gcp stack agent

--- .buildkite/pipeline.trigger.integration.tests.sh
+++ .buildkite/pipeline.trigger.integration.tests.sh
@@ -81,7 +81,7 @@ pushd test/packages/parallel > /dev/null
 for independent_agent in false; do
 for package in $(find . -maxdepth 1 -mindepth 1 -type d) ; do
     package_name=$(basename "${package}")
-    if [[ "$package_name" == "aws" || "$package_name" == "aws_logs" || "$package_name" == "gcp" ]] ; then
+    if [[ "$package_name" == "aws" || "$package_name" == "aws_logs" ]] ; then
         echoerr "Skip temporarily ${package_name}"
         continue
     fi
