537ce34917f803fe1e8297087481a0853dcc4658
Panos Koutsovasilis <panos.koutsovasilis@elastic.co>
2024-03-26 13:34:01 +0200

    feat: deprecate support for docker compose v1

--- internal/compose/compose.go
+++ internal/compose/compose.go
@@ -47,7 +47,6 @@ type Project struct {
 	name             string
 	composeFilePaths []string
 
-	dockerComposeV1                bool
 	dockerComposeStandalone        bool
 	disableANSI                    bool
 	disablePullProgressInformation bool
@@ -199,20 +198,19 @@ func NewProject(name string, paths ...string) (*Project, error) {
 	// Passing a nil context here because we are on initialization.
 	ver, err := c.dockerComposeVersion(context.Background())
 	if err != nil {
-		logger.Errorf("Unable to determine Docker Compose version: %v. Defaulting to 1.x", err)
-		c.dockerComposeV1 = true
+		logger.Errorf("Unable to determine Docker Compose version: %v. Defaulting to 2.x", err)
 		return &c, nil
 	}
 
 	versionMessage := fmt.Sprintf("Determined Docker Compose version: %v", ver)
-	if ver.Major() == 1 {
-		versionMessage = fmt.Sprintf("%s, the tool will use Compose V1", versionMessage)
-		c.dockerComposeV1 = true
-	}
 	logger.Debug(versionMessage)
 
+	if ver.Major() == 1 {
+		return nil, fmt.Errorf("the docker compose version %v is not supported", ver)
+	}
+
 	v, ok = os.LookupEnv(DisableVerboseOutputComposeEnv)
-	if !c.dockerComposeV1 && ok && strings.ToLower(v) != "false" {
+	if ok && strings.ToLower(v) != "false" {
 		if c.composeVersion.LessThan(semver.MustParse("2.19.0")) {
 			c.disableANSI = true
 		} else {
@@ -543,8 +541,5 @@ func (p *Project) dockerComposeVersion(ctx context.Context) (*semver.Version, er
 
 // ContainerName method the container name for the service.
 func (p *Project) ContainerName(serviceName string) string {
-	if p.dockerComposeV1 {
-		return fmt.Sprintf("%s_%s_1", p.name, serviceName)
-	}
 	return fmt.Sprintf("%s-%s-1", p.name, serviceName)
 }
