3565d6e018aaef69bc1ad484ca851c06250abf70
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2024-04-26 13:07:15 +0200

    Add support for clones created with git worktree

--- internal/files/repository.go
+++ internal/files/repository.go
@@ -5,10 +5,32 @@
 package files
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"path/filepath"
+
+	"gopkg.in/yaml.v3"
 )
+
+func isGitWorktree(path string) error {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return fmt.Errorf("failed to read %s: %w", path, err)
+	}
+	type gitWorktree struct {
+		GitDir string `yaml:"gitdir"`
+	}
+	var worktree gitWorktree
+	dec := yaml.NewDecoder(bytes.NewBuffer(content))
+	dec.KnownFields(true)
+
+	if err := dec.Decode(&worktree); err != nil {
+		return fmt.Errorf("failed to decode %s: %w", path, err)
+	}
+
+	return nil
+}
 
 func FindRepositoryRootDirectory() (string, error) {
 	workDir, err := os.Getwd()
@@ -24,6 +46,14 @@ func FindRepositoryRootDirectory() (string, error) {
 	for dir != "." {
 		path := filepath.Join(dir, ".git")
 		fileInfo, err := os.Stat(path)
+		if err == nil && !fileInfo.IsDir() {
+			errWorktree := isGitWorktree(path)
+			if errWorktree != nil {
+				return "", errWorktree
+			}
+
+			return dir, nil
+		}
 		if err == nil && fileInfo.IsDir() {
 			return dir, nil
 		}
