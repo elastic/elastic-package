827224c4b32b4b6b972d3a383b35082811ebbbfc
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2024-05-08 18:47:50 +0200

    Support decoding JSONs in embedded panels in dashboards

--- internal/builder/dashboards.go
+++ internal/builder/dashboards.go
@@ -13,14 +13,21 @@ import (
 	"github.com/elastic/elastic-package/internal/common"
 )
 
+const (
+	panelsAttribute           = "attributes.panelsJSON"
+	embeddableConfigAttribute = "embeddableConfig"
+)
+
 var fieldsToEncode = []string{
+	"attributes.controlGroupInput.ignoreParentSettingsJSON",
+	"attributes.controlGroupInput.panelsJSON",
 	"attributes.kibanaSavedObjectMeta.searchSourceJSON",
 	"attributes.layerListJSON",
 	"attributes.mapStateJSON",
 	"attributes.optionsJSON",
-	"attributes.panelsJSON",
 	"attributes.uiStateJSON",
 	"attributes.visState",
+	panelsAttribute,
 }
 
 func encodeDashboards(destinationDir string) error {
@@ -29,14 +36,13 @@ func encodeDashboards(destinationDir string) error {
 		return err
 	}
 	for _, file := range savedObjects {
-
 		data, err := os.ReadFile(file)
 		if err != nil {
 			return err
 		}
-		output, changed, err := encodedSavedObject(data)
+		output, changed, err := encodeSavedObject(data)
 		if err != nil {
-			return err
+			return fmt.Errorf("encoding %s: %w", file, err)
 		}
 
 		if changed {
@@ -53,16 +59,28 @@ func encodeDashboards(destinationDir string) error {
 // which are stored in encoded JSON in Kibana.
 // The reason is that for versioning it is much nicer to have the full
 // json so only on packaging this is changed.
-func encodedSavedObject(data []byte) ([]byte, bool, error) {
+func encodeSavedObject(data []byte) ([]byte, bool, error) {
 	savedObject := common.MapStr{}
 	err := json.Unmarshal(data, &savedObject)
 	if err != nil {
 		return nil, false, fmt.Errorf("unmarshalling saved object failed: %w", err)
 	}
 
-	var changed bool
+	object, changed, err := encodeObjectMapStr(savedObject)
+	if err != nil {
+		return nil, false, err
+	}
+
+	return []byte(object.StringToPrint()), changed, nil
+}
+
+func encodeObjectMapStr(object common.MapStr) (common.MapStr, bool, error) {
+	object, changed, err := encodeEmbeddedPanels(object)
+	if err != nil {
+		return nil, false, err
+	}
 	for _, v := range fieldsToEncode {
-		out, err := savedObject.GetValue(v)
+		out, err := object.GetValue(v)
 		// This means the key did not exists, no conversion needed.
 		if err != nil {
 			continue
@@ -80,11 +98,54 @@ func encodedSavedObject(data []byte) ([]byte, bool, error) {
 		if err != nil {
 			return nil, false, err
 		}
-		_, err = savedObject.Put(v, string(r))
+		_, err = object.Put(v, string(r))
 		if err != nil {
 			return nil, false, fmt.Errorf("can't put value to the saved object: %w", err)
 		}
 		changed = true
 	}
-	return []byte(savedObject.StringToPrint()), changed, nil
+	return object, changed, nil
+}
+
+func encodeEmbeddedPanels(object common.MapStr) (common.MapStr, bool, error) {
+	embeddedPanelsValue, err := object.GetValue(panelsAttribute)
+	if err == common.ErrKeyNotFound {
+		return object, false, nil
+	}
+	if err != nil {
+		return nil, false, fmt.Errorf("retrieving embedded panels failed: %w", err)
+	}
+	embeddedPanels, ok := embeddedPanelsValue.([]any)
+	if !ok {
+		return nil, false, fmt.Errorf("expected list of panels, found %T", embeddedPanelsValue)
+	}
+
+	changed := false
+	for i, panelValue := range embeddedPanels {
+		panel, ok := panelValue.(map[string]any)
+		if !ok {
+			return nil, false, fmt.Errorf("expected panel in map format, found %T", panel)
+		}
+		embeddableConfigValue, ok := panel[embeddableConfigAttribute]
+		if !ok {
+			continue
+		}
+		embeddableConfig, ok := embeddableConfigValue.(map[string]any)
+		if !ok {
+			return nil, false, fmt.Errorf("embeddable config is not a map, found %T", embeddableConfigValue)
+		}
+		embeddableConfig, embeddableChanged, err := encodeObjectMapStr(common.MapStr(embeddableConfig))
+		if err != nil {
+			return nil, false, fmt.Errorf("econdign embedded object failed: %w", err)
+		}
+		if embeddableChanged {
+			changed = true
+		}
+
+		panel[embeddableConfigAttribute] = embeddableConfig
+		embeddedPanels[i] = panel
+	}
+	object.Put(panelsAttribute, embeddedPanels)
+
+	return object, changed, nil
 }
--- internal/export/transform_decode.go
+++ internal/export/transform_decode.go
@@ -11,17 +11,22 @@ import (
 	"github.com/elastic/elastic-package/internal/common"
 )
 
-var (
-	encodedFields = []string{
-		"attributes.kibanaSavedObjectMeta.searchSourceJSON",
-		"attributes.layerListJSON",
-		"attributes.mapStateJSON",
-		"attributes.optionsJSON",
-		"attributes.panelsJSON",
-		"attributes.uiStateJSON",
-		"attributes.visState",
-	}
+const (
+	panelsAttribute           = "attributes.panelsJSON"
+	embeddableConfigAttribute = "embeddableConfig"
 )
+
+var encodedFields = []string{
+	"attributes.controlGroupInput.ignoreParentSettingsJSON",
+	"attributes.controlGroupInput.panelsJSON",
+	"attributes.kibanaSavedObjectMeta.searchSourceJSON",
+	"attributes.layerListJSON",
+	"attributes.mapStateJSON",
+	"attributes.optionsJSON",
+	"attributes.uiStateJSON",
+	"attributes.visState",
+	panelsAttribute,
+}
 
 func decodeObject(ctx *transformationContext, object common.MapStr) (common.MapStr, error) {
 	for _, fieldToDecode := range encodedFields {
@@ -51,5 +56,44 @@ func decodeObject(ctx *transformationContext, object common.MapStr) (common.MapS
 			return nil, fmt.Errorf("can't update field (key: %s): %w", fieldToDecode, err)
 		}
 	}
+
+	object, err := decodeEmbeddedPanels(ctx, object)
+	if err != nil {
+		return nil, err
+	}
+	return object, nil
+}
+
+func decodeEmbeddedPanels(ctx *transformationContext, object common.MapStr) (common.MapStr, error) {
+	embeddedPanelsValue, err := object.GetValue(panelsAttribute)
+	if err == common.ErrKeyNotFound {
+		return object, nil
+	}
+	if err != nil {
+		return nil, fmt.Errorf("retrieving embedded panels failed: %w", err)
+	}
+	embeddedPanels, ok := embeddedPanelsValue.([]map[string]any)
+	if !ok {
+		return nil, fmt.Errorf("expected list of panels, found %T", embeddedPanelsValue)
+	}
+	for i, panel := range embeddedPanels {
+		embeddableConfigValue, ok := panel[embeddableConfigAttribute]
+		if !ok {
+			continue
+		}
+		embeddableConfig, ok := embeddableConfigValue.(map[string]any)
+		if !ok {
+			return nil, fmt.Errorf("embeddable config is not a map, found %T", embeddableConfigValue)
+		}
+		embeddableConfig, err = decodeObject(ctx, common.MapStr(embeddableConfig))
+		if err != nil {
+			return nil, fmt.Errorf("decoding embedded object failed: %w", err)
+		}
+
+		panel[embeddableConfigAttribute] = embeddableConfig
+		embeddedPanels[i] = panel
+	}
+	object.Put(panelsAttribute, embeddedPanels)
+
 	return object, nil
 }
