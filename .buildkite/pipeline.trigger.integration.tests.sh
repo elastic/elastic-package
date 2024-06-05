de0cc7946eb611301b3b88f5a6defc9dd7e1543d
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2024-06-05 19:12:29 +0200

    Set just one parallel tests for kind with independent agents

--- .buildkite/pipeline.trigger.integration.tests.sh
+++ .buildkite/pipeline.trigger.integration.tests.sh
@@ -52,10 +52,6 @@ for test in "${CHECK_PACKAGES_TESTS[@]}"; do
     test_name=${test#"test-check-packages-"}
     echo "      - label: \":go: Integration test: ${test_name}${label_suffix}\""
     echo "        command: ./.buildkite/scripts/integration_tests.sh -t ${test}"
-    if [[ "$test" == "test-check-packages-with-kind" ]]; then
-        echo "        env:"
-        echo "          ELASTIC_PACKAGE_MAXIMUM_NUMBER_PARALLEL_TESTS: 1"
-    fi
     echo "        agents:"
     echo "          provider: \"gcp\""
     echo "          image: \"${IMAGE_UBUNTU_X86_64}\""
@@ -67,9 +63,14 @@ for test in "${CHECK_PACKAGES_TESTS[@]}"; do
     if [[ $test =~ with-kind$ ]]; then
         echo "          - build/kubectl-dump.txt"
     fi
-    if [[ "${independent_agent}" == "true" ]]; then
+    if [[ "${independent_agent}" == "true" || "$test_name" == "with-kind" ]]; then
         echo "        env:"
-        echo "          ELASTIC_PACKAGE_TEST_ENABLE_INDEPENDENT_AGENT: ${independent_agent}"
+        if [[ "${independent_agent}" == "true" ]]; then
+            echo "          ELASTIC_PACKAGE_TEST_ENABLE_INDEPENDENT_AGENT: ${independent_agent}"
+        fi
+        if [[ "$test_name" == "with-kind" ]]; then
+                echo "          ELASTIC_PACKAGE_MAXIMUM_NUMBER_PARALLEL_TESTS: 1"
+        fi
     fi
 done
 done
