d159d3050bb5a4820e1711eed18d60292a71cfd4
Hanna Tamoudi <hanna.tamoudi@elastic.co>
2024-09-02 17:54:19 +0200

    add test cobertura for single pipeline

--- internal/testrunner/runners/pipeline/coverage_test.go
+++ internal/testrunner/runners/pipeline/coverage_test.go
@@ -143,3 +143,126 @@ func TestGenericCoverageForSinglePipeline(t *testing.T) {
 		})
 	}
 }
+
+func TestCoberturaForSinglePipeline(t *testing.T) {
+	const firstProcessorName = "append"
+	const firstProcessorFirstLine = 1
+	const firstProcessorHitCount = 13
+
+	const secondProcessorName = "geoip"
+	const secondProcessorFirstLine = 2
+	const secondProcessorHitCount = 17
+
+	const pipelineName = "Pipeline name"
+
+	for _, testcase := range []struct {
+		title                string
+		pipelineName         string
+		pipelineRelPath      string
+		src                  []ingest.Processor
+		pstats               ingest.PipelineStats
+		expectedLinesCovered int64
+		expectedClass        *testrunner.CoberturaClass
+	}{
+		{
+			title:           "Single Processor - covered",
+			pipelineName:    pipelineName,
+			pipelineRelPath: "",
+			src: []ingest.Processor{
+				{Type: firstProcessorName, FirstLine: firstProcessorFirstLine, LastLine: firstProcessorFirstLine},
+			},
+			pstats: ingest.PipelineStats{
+				StatsRecord: ingest.StatsRecord{
+					Count:        9,
+					Current:      10,
+					Failed:       11,
+					TimeInMillis: 12,
+				},
+				Processors: []ingest.ProcessorStats{
+					{
+						Type:        firstProcessorName,
+						Conditional: true,
+						Stats: ingest.StatsRecord{
+							Count:        firstProcessorHitCount,
+							Current:      14,
+							Failed:       15,
+							TimeInMillis: 16,
+						},
+					},
+				},
+			},
+			expectedLinesCovered: 1,
+			expectedClass: &testrunner.CoberturaClass{
+				Name: pipelineName,
+				Lines: []*testrunner.CoberturaLine{
+					{Number: firstProcessorFirstLine, Hits: firstProcessorHitCount},
+				},
+				Methods: []*testrunner.CoberturaMethod{
+					{Name: firstProcessorName, Lines: []*testrunner.CoberturaLine{
+						{Number: firstProcessorFirstLine, Hits: firstProcessorHitCount},
+					}},
+				},
+			},
+		},
+		{
+			title:           "Multi Processor - covered",
+			pipelineRelPath: "",
+			src: []ingest.Processor{
+				{Type: firstProcessorName, FirstLine: firstProcessorFirstLine, LastLine: firstProcessorFirstLine},
+				{Type: secondProcessorName, FirstLine: secondProcessorFirstLine, LastLine: secondProcessorFirstLine},
+			},
+			pstats: ingest.PipelineStats{
+				StatsRecord: ingest.StatsRecord{
+					Count:        9,
+					Current:      10,
+					Failed:       11,
+					TimeInMillis: 12,
+				},
+				Processors: []ingest.ProcessorStats{
+					{
+						Type:        firstProcessorName,
+						Conditional: true,
+						Stats: ingest.StatsRecord{
+							Count:        firstProcessorHitCount,
+							Current:      14,
+							Failed:       15,
+							TimeInMillis: 16,
+						},
+					},
+					{
+						Type: secondProcessorName,
+						Stats: ingest.StatsRecord{
+							Count:        secondProcessorHitCount,
+							Current:      18,
+							Failed:       19,
+							TimeInMillis: 20,
+						},
+					},
+				},
+			},
+			expectedLinesCovered: 2,
+			expectedClass: &testrunner.CoberturaClass{
+				Name: pipelineName,
+				Lines: []*testrunner.CoberturaLine{
+					{Number: firstProcessorFirstLine, Hits: firstProcessorHitCount},
+					{Number: secondProcessorFirstLine, Hits: secondProcessorHitCount},
+				},
+				Methods: []*testrunner.CoberturaMethod{
+					{Name: firstProcessorName, Lines: []*testrunner.CoberturaLine{
+						{Number: firstProcessorFirstLine, Hits: firstProcessorHitCount},
+					}},
+
+					{Name: secondProcessorFirstLine, Lines: []*testrunner.CoberturaLine{
+						{Number: secondProcessorName, Hits: secondProcessorHitCount},
+					}},
+				},
+			},
+		},
+	} {
+		t.Run(testcase.title, func(t *testing.T) {
+			linesCoveredResult, classResult, _ := coberturaForSinglePipeline(testcase.pipelineName, testcase.pipelineRelPath, testcase.src, testcase.pstats)
+			assert.Equal(t, testcase.expectedLinesCovered, linesCoveredResult)
+			assert.Equal(t, testcase.expectedClass, classResult)
+		})
+	}
+}
