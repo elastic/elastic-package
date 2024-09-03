67ab64d371ebb7c217abc274106383d32db4700e
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2024-09-03 18:03:34 +0200

    Accept as numbers strings that can be parsed as numbers (#2065)
    Similar to `numeric_keyword_fields`, add a new `string_number_fields` setting to
    pipeline and system tests. With this setting, validation of fields can accept strings
    as numbers if they can be parsed as float. This mimics the behaviour of Elasticsearch,
    that is able to accept strings as numbers if they can be parsed like them.
    
    Refactor `numeric_keyword_fields`, so it is applied when validating values, instead of
    modifying the original value.

--- internal/fields/validate_test.go
+++ internal/fields/validate_test.go
@@ -82,6 +82,22 @@ func TestValidate_WithNumericKeywordFields(t *testing.T) {
 	require.Empty(t, errs)
 }
 
+func TestValidate_WithStringNumberFields(t *testing.T) {
+	validator, err := CreateValidatorForDirectory("testdata",
+		WithStringNumberFields([]string{
+			"foo.count",  // Contains a number as string.
+			"foo.metric", // Contains a floating number as string.
+		}),
+		WithSpecVersion("2.3.0"), // Needed to validate normalization.
+		WithDisabledDependencyManagement())
+	require.NoError(t, err)
+	require.NotNil(t, validator)
+
+	e := readSampleEvent(t, "testdata/stringnumbers.json")
+	errs := validator.ValidateDocumentBody(e)
+	require.Empty(t, errs)
+}
+
 func TestValidate_WithEnabledImportAllECSSchema(t *testing.T) {
 	finder := packageRootTestFinder{"../../test/packages/other/imported_mappings_tests"}
 
@@ -344,14 +360,6 @@ func Test_parseElementValue(t *testing.T) {
 			},
 		},
 		{
-			key:   "numeric string array to long",
-			value: []any{"123", "42"},
-			definition: FieldDefinition{
-				Type: "long",
-			},
-			fail: true,
-		},
-		{
 			key:   "mixed numbers and strings in number array",
 			value: []any{123, "hi"},
 			definition: FieldDefinition{
@@ -527,15 +535,6 @@ func Test_parseElementValue(t *testing.T) {
 			definition: FieldDefinition{
 				Type: "float",
 			},
-		},
-		// long
-		{
-			key:   "bad long",
-			value: "65537",
-			definition: FieldDefinition{
-				Type: "long",
-			},
-			fail: true,
 		},
 		// allowed values
 		{
