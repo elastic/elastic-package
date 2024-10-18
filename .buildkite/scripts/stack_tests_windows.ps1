ce4e94028a9af60be3a983d53da647f5daee9d78
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2024-10-18 13:50:36 +0200

    Update comments

--- .buildkite/scripts/stack_tests_windows.ps1
+++ .buildkite/scripts/stack_tests_windows.ps1
@@ -56,11 +56,13 @@ go mod download -x
 echo "--- Running stack tests"
 $ErrorActionPreference = "Continue" # set +e
 
-# TODO: stack status checks that we can call docker-compose, but we should try a stack up.
+# TODO: stack status checks that we can call docker, but we should try a stack up to try also with docker-compose with a full scenario.
 # stack up doesn't work because we didn't manage to enable the linux engine, and we don't have Windows native images.
 echo "Stack Status"
 go run . stack status -v
 echo "Stack up"
+# running this stack up command adds the required files under ~/.elastic-package to run afterwards "elastic-package stack down" successfully
+# that uses docker-compose under the hood
 go run . stack up -v -d
 echo "Stack Status"
 go run . stack status -v
