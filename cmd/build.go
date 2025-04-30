6c549e19e10c2d52b432d8800816c5c07f63cc73
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2025-04-30 18:21:28 +0200

    Fix context for elastic-package check

--- cmd/build.go
+++ cmd/build.go
@@ -5,6 +5,7 @@
 package cmd
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"path/filepath"
@@ -82,7 +83,12 @@ func buildCommandAction(cmd *cobra.Command, args []string) error {
 		cmd.Printf("%s file rendered: %s\n", fileName, target)
 	}
 
-	target, err := builder.BuildPackage(cmd.Context(), builder.BuildOptions{
+	ctx := cmd.Context()
+	if ctx == nil {
+		// FIXME: when executging elastic-package check , context returned is nil
+		ctx = context.Background()
+	}
+	target, err := builder.BuildPackage(ctx, builder.BuildOptions{
 		PackageRoot:    packageRoot,
 		CreateZip:      createZip,
 		SignPackage:    signPackage,
