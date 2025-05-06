76c93ea73339e2665b7b8cb68a38ed62e9d8768f
Victor Martinez <VictorMartinezRubio@gmail.com>
2025-05-06 22:10:27 +0200

    fix

--- .buildkite/scripts/test-oidc.sh
+++ .buildkite/scripts/test-oidc.sh
@@ -9,7 +9,7 @@ FALSE_POSITIVES_TARGET="test-check-packages-false-positives"
 KIND_TARGET="test-check-packages-with-kind"
 SYSTEM_TEST_FLAGS_TARGET="test-system-test-flags"
 TEST_BUILD_ZIP_TARGET="test-build-zip"
-
+export JOB_GCS_BUCKET_INTERNAL="ecosystem-ci-internal"
 REPO_NAME=$(repo_name "${BUILDKITE_REPO}")
 export REPO_BUILD_TAG="${REPO_NAME}/$(buildkite_pr_branch_build_id)"
 mkdir -p build/elastic-stack-dump
