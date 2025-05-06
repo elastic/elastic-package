d06931a481e16920da7be0e90def7f5a993ef817
Victor Martinez <VictorMartinezRubio@gmail.com>
2025-05-06 14:47:02 +0200

    print errors

--- .buildkite/scripts/tooling.sh
+++ .buildkite/scripts/tooling.sh
@@ -34,7 +34,7 @@ buildkite_pr_branch_build_id() {
 google_cloud_auth() {
     local keyFile=$1
 
-    gcloud auth activate-service-account --key-file "${keyFile}" 2> /dev/null
+    gcloud auth activate-service-account --key-file "${keyFile}"
 }
 
 retry() {
@@ -110,7 +110,7 @@ upload_safe_logs() {
     local source="$2"
     local target="$3"
 
-    echo "Uploading safe logs to GCP bucket ${bucket}"
+    echo "--- Uploading safe logs to GCP bucket ${bucket}"
 
     if ! ls ${source} 2>&1 > /dev/null ; then
         echo "upload_safe_logs: artifacts files not found, nothing will be archived"
