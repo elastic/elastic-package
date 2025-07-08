2ab17034c2163fd99cc6b038f32bf7b1dbe8125d
Marc Guasch <marc.guasch@elastic.co>
2025-07-08 10:53:02 +0200

    check

--- internal/files/linkedfiles.go
+++ internal/files/linkedfiles.go
@@ -61,13 +61,13 @@ func NewLinksFS(repoRoot *os.Root, workDir string) (*LinksFS, error) {
 	// Ensure workDir is absolute for os.DirFS
 	var absWorkDir string
 	var err error
-	
+
 	if filepath.IsAbs(workDir) {
 		absWorkDir = workDir
 	} else {
 		// First try: assume workDir is relative to the repository root
 		absWorkDir = filepath.Join(repoRoot.Name(), workDir)
-		
+
 		// Check if path exists
 		if _, err := os.Stat(absWorkDir); os.IsNotExist(err) {
 			// Second try: path might be relative to current working directory
@@ -88,7 +88,7 @@ func NewLinksFS(repoRoot *os.Root, workDir string) (*LinksFS, error) {
 	if err != nil {
 		return nil, fmt.Errorf("could not validate workDir %s: %w", absWorkDir, err)
 	}
-	
+
 	if !inRoot {
 		return nil, fmt.Errorf("workDir %s is outside the repository root %s", absWorkDir, repoRoot.Name())
 	}
