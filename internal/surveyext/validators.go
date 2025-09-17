3725f12d4308a2638d4ad9b77555a69324f9f911
Michael Wolf <michael.wolf@elastic.co>
2025-09-17 14:30:15 -0700

    Remove unused function

--- internal/surveyext/validators.go
+++ internal/surveyext/validators.go
@@ -12,7 +12,6 @@ import (
 	"regexp"
 
 	"github.com/Masterminds/semver/v3"
-	"github.com/elastic/elastic-package/internal/tui"
 )
 
 var (
@@ -24,11 +23,6 @@ var (
 
 type Validator struct {
 	Cwd string
-}
-
-// Convert validator methods to be compatible with tui.Validator
-func (v Validator) toTUIValidator(validatorFunc func(interface{}) error) tui.Validator {
-	return tui.Validator(validatorFunc)
 }
 
 // PackageDoesNotExist function checks if the package hasn't been already created.
