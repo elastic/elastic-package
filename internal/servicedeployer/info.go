856ea5a3c65f77bd691448e38ea99a3d4dd95b77
Dan Kortschak <dan.kortschak@elastic.co>
2025-09-18 07:42:13 +0930

    internal/servicedeployer: expose environment, and project identifier and construction

--- internal/servicedeployer/info.go
+++ internal/servicedeployer/info.go
@@ -4,6 +4,8 @@
 
 package servicedeployer
 
+import "fmt"
+
 const (
 	localCACertEnv      = "LOCAL_CA_CERT"
 	serviceLogsDirEnv   = "SERVICE_LOGS_DIR"
@@ -94,4 +96,9 @@ func (sc *ServiceInfo) Aliases() map[string]interface{} {
 		}
 	}
 	return m
+}
+
+// Project returns the project identifier for the service.
+func (sc *ServiceInfo) Project() string {
+	return fmt.Sprintf("elastic-package-service-%s", sc.Test.RunID)
 }
