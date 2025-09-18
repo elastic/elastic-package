1aa7b3188ab02321448f3755191dda9f7285b1d9
Tere <teresa.romero@elastic.co>
2025-09-18 10:44:43 +0200

    Conditional subobjects based on package version (#2911)
    * add support for version-based question prompts in data stream creation
    
    * Update cmd/create_data_stream.go
    
    Co-authored-by: Jaime Soriano Pastor <jaime.soriano@elastic.co>
    
    * extract survey question logic into a separate function and add tests
    
    * add tests for createDataStreamDescriptorFromAnswers handling of Subobjects based on spec version
    
    * add copyright notice to create_data_stream_test.go
    
    * Apply suggestion from @mrodm
    
    Co-authored-by: Mario Rodriguez Molins <marrodmo@gmail.com>
    
    * Apply suggestion from @mrodm
    
    Co-authored-by: Mario Rodriguez Molins <marrodmo@gmail.com>
    
    * Apply suggestion from @mrodm
    
    Co-authored-by: Mario Rodriguez Molins <marrodmo@gmail.com>
    
    * Apply suggestion from @mrodm
    
    Co-authored-by: Mario Rodriguez Molins <marrodmo@gmail.com>
    
    * Apply suggestion from @mrodm
    
    Co-authored-by: Mario Rodriguez Molins <marrodmo@gmail.com>
    
    * Apply suggestion from @mrodm
    
    Co-authored-by: Mario Rodriguez Molins <marrodmo@gmail.com>
    
    * Add missing require import in create_data_stream_test.go
    
    * Fix assertion for subobjects true case
    
    ---------
    
    Co-authored-by: Jaime Soriano Pastor <jaime.soriano@elastic.co>
    Co-authored-by: Mario Rodriguez Molins <marrodmo@gmail.com>

---
+++ cmd/create_data_stream_test.go
@@ -0,0 +1,94 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package cmd
+
+import (
+	"testing"
+
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/Masterminds/semver/v3"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetSurveyQuestionsForVersion_BelowSemver3_2_0(t *testing.T) {
+	version := semver.MustParse("3.1.9")
+	questions := getInitialSurveyQuestionsForVersion(version)
+
+	require.Len(t, questions, 3, "should return 3 questions for spec version < 3.2.0")
+
+	assert.Equal(t, "name", questions[0].Name)
+	assert.IsType(t, &survey.Input{}, questions[0].Prompt)
+	assert.Equal(t, "title", questions[1].Name)
+	assert.IsType(t, &survey.Input{}, questions[1].Prompt)
+	assert.Equal(t, "type", questions[2].Name)
+	assert.IsType(t, &survey.Select{}, questions[2].Prompt)
+}
+
+func TestGetSurveyQuestionsForVersion_EqualSemver3_2_0(t *testing.T) {
+	version := semver.MustParse("3.2.0")
+	questions := getInitialSurveyQuestionsForVersion(version)
+
+	require.Len(t, questions, 4, "should return 4 questions for spec version >= 3.2.0")
+
+	assert.Equal(t, "subobjects", questions[3].Name)
+	assert.IsType(t, &survey.Confirm{}, questions[3].Prompt)
+}
+
+func TestGetSurveyQuestionsForVersion_AboveSemver3_2_0(t *testing.T) {
+	version := semver.MustParse("3.3.0")
+	questions := getInitialSurveyQuestionsForVersion(version)
+
+	require.Len(t, questions, 4, "should return 4 questions for spec version > 3.2.0")
+
+	assert.Equal(t, "subobjects", questions[3].Name)
+	assert.IsType(t, &survey.Confirm{}, questions[3].Prompt)
+}
+
+func TestCreateDataStreamDescriptorFromAnswers_SubobjectsFalseForSpecVersionBelow3_2_0(t *testing.T) {
+	specVersion := semver.MustParse("3.1.0")
+	answers := newDataStreamAnswers{
+		Name:       "test_stream",
+		Title:      "Test Stream",
+		Type:       "logs",
+		Subobjects: false,
+	}
+	descriptor := createDataStreamDescriptorFromAnswers(answers, "/tmp/package", specVersion)
+
+	assert.Equal(t, "test_stream", descriptor.Manifest.Name)
+	assert.Equal(t, "Test Stream", descriptor.Manifest.Title)
+	assert.Equal(t, "logs", descriptor.Manifest.Type)
+	assert.Equal(t, "/tmp/package", descriptor.PackageRoot)
+	assert.Nil(t, descriptor.Manifest.Elasticsearch)
+}
+
+func TestCreateDataStreamDescriptorFromAnswers_SubobjectsFalseForSpecVersionGTE3_2_0(t *testing.T) {
+	specVersion := semver.MustParse("3.2.0")
+	answers := newDataStreamAnswers{
+		Name:       "test_stream",
+		Title:      "Test Stream",
+		Type:       "logs",
+		Subobjects: false,
+	}
+	descriptor := createDataStreamDescriptorFromAnswers(answers, "/tmp/package", specVersion)
+
+	require.NotNil(t, descriptor.Manifest.Elasticsearch)
+	require.NotNil(t, descriptor.Manifest.Elasticsearch.IndexTemplate)
+	require.NotNil(t, descriptor.Manifest.Elasticsearch.IndexTemplate.Mappings)
+	assert.False(t, descriptor.Manifest.Elasticsearch.IndexTemplate.Mappings.Subobjects)
+}
+
+func TestCreateDataStreamDescriptorFromAnswers_SubobjectsTrueForSpecVersionGTE3_2_0(t *testing.T) {
+	specVersion := semver.MustParse("3.2.0")
+	answers := newDataStreamAnswers{
+		Name:       "test_stream",
+		Title:      "Test Stream",
+		Type:       "logs",
+		Subobjects: true,
+	}
+	descriptor := createDataStreamDescriptorFromAnswers(answers, "/tmp/package", specVersion)
+
+	require.Nil(t, descriptor.Manifest.Elasticsearch)
+}
