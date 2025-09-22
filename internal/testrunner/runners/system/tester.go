cc93829c8122c1d174a6b29081e3cf8b7cfa8457
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2025-09-22 12:56:14 +0200

    Remove skip fields for OTEL
    Packages using OTEL collector input will perform the validation based on
    the mappings found before and after ingesting documents.

--- internal/testrunner/runners/system/tester.go
+++ internal/testrunner/runners/system/tester.go
@@ -1639,7 +1639,6 @@ func (r *tester) validateTestScenario(ctx context.Context, result *testrunner.Re
 		fields.WithExpectedDatasets(expectedDatasets),
 		fields.WithEnabledImportAllECSSChema(true),
 		fields.WithDisableNormalization(scenario.syntheticEnabled),
-		fields.WithOTELValidation(r.pkgManifest.Type == "input" && scenario.policyTemplateInput == otelCollectorInputName),
 	)
 	if err != nil {
 		return result.WithErrorf("creating fields validator for data stream failed (path: %s): %w", r.dataStreamPath, err)
