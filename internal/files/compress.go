5b63c17688fdeb554396ca2609ef9c7a37bff743
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2025-11-10 19:14:10 +0100

    Clone zip.AddFS instead of adding a wrapper

--- internal/files/compress.go
+++ internal/files/compress.go
@@ -6,8 +6,12 @@ package files
 
 import (
 	"archive/zip"
+	"errors"
 	"fmt"
+	"io"
+	"io/fs"
 	"os"
+	"path"
 	"path/filepath"
 	"strings"
 )
@@ -22,8 +26,7 @@ func Zip(sourcePath, destinationFile string) error {
 
 	folderName := folderNameFromFileName(destinationFile)
 	z := zip.NewWriter(out)
-	fs := newFSWithPrefix(os.DirFS(sourcePath), folderName)
-	err = z.AddFS(fs)
+	err = addFSWithPrefix(z, os.DirFS(sourcePath), folderName)
 	if err != nil {
 		return fmt.Errorf("failed to add files to package zip: %w", err)
 	}
@@ -44,4 +47,50 @@ func folderNameFromFileName(filename string) string {
 		return base[:firstDot]
 	}
 	return base
+}
+
+// addFSWithPrefix adds the files from fs.FS to the archive adding as a first folder of the zip the given package (e.g. nginx-1.0.0)
+// Implementation based on AddFS method from archive/zip package in Go 1.20+
+// https://cs.opensource.google/go/go/+/refs/tags/go1.25.4:src/archive/zip/writer.go;l=503
+func addFSWithPrefix(zw *zip.Writer, fsys fs.FS, prefix string) error {
+	return fs.WalkDir(fsys, ".", func(name string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if name == "." {
+			return nil
+		}
+		info, err := d.Info()
+		if err != nil {
+			return err
+		}
+		if !d.IsDir() && !info.Mode().IsRegular() {
+			return errors.New("zip: cannot add non-regular file")
+		}
+		h, err := zip.FileInfoHeader(info)
+		if err != nil {
+			return err
+		}
+
+		// Set the path containing the base folder within the zip file
+		h.Name = path.Join(prefix, name)
+		if d.IsDir() {
+			h.Name += "/"
+		}
+		h.Method = zip.Deflate
+		fw, err := zw.CreateHeader(h)
+		if err != nil {
+			return err
+		}
+		if d.IsDir() {
+			return nil
+		}
+		f, err := fsys.Open(name)
+		if err != nil {
+			return fmt.Errorf("failed to open file: %w", err)
+		}
+		defer f.Close()
+		_, err = io.Copy(fw, f)
+		return err
+	})
 }
