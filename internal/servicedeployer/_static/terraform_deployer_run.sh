c10fd3f63501a9683eaaec0ef6cb3506a4ebdc7d
Mario Rodriguez Molins <mario.rodriguez@elastic.co>
2026-01-13 19:45:48 +0100

    Debug files

--- internal/servicedeployer/_static/terraform_deployer_run.sh
+++ internal/servicedeployer/_static/terraform_deployer_run.sh
@@ -6,6 +6,7 @@ set -euxo pipefail
 # See more: https://github.com/elastic/elastic-package/pull/603
 # NOTE: must copy hidden files too (supported by "/.")
 # See more: https://github.com/elastic/package-spec/issues/269
+
 cp -r /stage/. /workspace
 
 cleanup() {
@@ -17,6 +18,9 @@ cleanup() {
   exit $r
 }
 trap cleanup EXIT INT TERM
+
+ls -l "${GOOGLE_APPLICATION_CREDENTIALS:-/dev/null}" || true # For debugging purposes
+ls -l /tmp/tmp.*/token.json || true # For debugging purposes
 
 terraform init
 terraform plan
