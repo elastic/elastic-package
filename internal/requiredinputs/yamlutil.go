8337f12aac5ab0992686b9b27ca0f2f34871d07a
Tere <teresa.romero@elastic.co>
2026-03-13 12:10:15 +0100

    Refactor YAML utility functions to use slices.IndexFunc for improved readability and performance

--- internal/requiredinputs/yamlutil.go
+++ internal/requiredinputs/yamlutil.go
@@ -6,6 +6,7 @@ package requiredinputs
 
 import (
 	"fmt"
+	"slices"
 
 	"gopkg.in/yaml.v3"
 
@@ -15,31 +16,33 @@ import (
 // mappingValue returns the value node for the given key in a YAML mapping node,
 // or nil if the key is not present.
 func mappingValue(node *yaml.Node, key string) *yaml.Node {
-	for i := 0; i+1 < len(node.Content); i += 2 {
-		if node.Content[i].Value == key {
-			return node.Content[i+1]
-		}
+	idx := slices.IndexFunc(node.Content, func(n *yaml.Node) bool {
+		return n.Value == key
+	})
+	if idx < 0 || idx+1 >= len(node.Content) {
+		return nil
 	}
-	return nil
+	return node.Content[idx+1]
 }
 
 // removeKey removes a key-value pair from a YAML mapping node.
 func removeKey(node *yaml.Node, key string) {
-	for i := 0; i+1 < len(node.Content); i += 2 {
-		if node.Content[i].Value == key {
-			node.Content = append(node.Content[:i], node.Content[i+2:]...)
-			return
-		}
+	idx := slices.IndexFunc(node.Content, func(n *yaml.Node) bool {
+		return n.Value == key
+	})
+	if idx >= 0 && idx+1 < len(node.Content) {
+		node.Content = slices.Delete(node.Content, idx, idx+2)
 	}
 }
 
 // upsertKey sets key to value in a YAML mapping node, adding it if absent.
 func upsertKey(node *yaml.Node, key string, value *yaml.Node) {
-	for i := 0; i+1 < len(node.Content); i += 2 {
-		if node.Content[i].Value == key {
-			node.Content[i+1] = value
-			return
-		}
+	idx := slices.IndexFunc(node.Content, func(n *yaml.Node) bool {
+		return n.Value == key
+	})
+	if idx >= 0 && idx+1 < len(node.Content) {
+		node.Content[idx+1] = value
+		return
 	}
 	keyNode := &yaml.Node{Kind: yaml.ScalarNode, Value: key}
 	node.Content = append(node.Content, keyNode, value)
