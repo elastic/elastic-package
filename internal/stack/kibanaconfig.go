fb94eb3912e6ff2b8875279d251ec5b3f052f85a
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2026-03-27 18:46:12 +0100

    Get content and log warning once

--- internal/stack/kibanaconfig.go
+++ internal/stack/kibanaconfig.go
@@ -5,9 +5,12 @@
 package stack
 
 import (
+	"errors"
 	"fmt"
 	"io"
+	"io/fs"
 	"os"
+	"sync"
 
 	"github.com/elastic/go-resource"
 
@@ -19,28 +22,44 @@ import (
 // (if present) to whatever was already written. The profile is retrieved from the
 // resource context so this source can be declared statically.
 func kibanaCustomContent() resource.FileContent {
+	// Reading the content only once to avoid printing log messages multiple times.
+	var once sync.Once
+	var content []byte
+	var contentErr error
 	return func(ctx resource.Context, w io.Writer) error {
-		var p *profile.Profile
-		if ok := ctx.Provider("profile", &p); !ok {
-			return fmt.Errorf("a profile is expected in the resource context")
-		}
+		once.Do(func() {
+			var p *profile.Profile
+			if ok := ctx.Provider("profile", &p); !ok {
+				contentErr = fmt.Errorf("a profile is expected in the resource context")
+				return
+			}
 
-		customConfigPath := p.Path(KibanaDevConfigFile)
-		customConfigData, err := os.ReadFile(customConfigPath)
-		if os.IsNotExist(err) {
+			customConfigPath := p.Path(KibanaDevConfigFile)
+			d, err := os.ReadFile(customConfigPath)
+			if errors.Is(err, fs.ErrNotExist) {
+				return
+			}
+			if err != nil {
+				contentErr = fmt.Errorf("failed to read custom kibana config: %w", err)
+				return
+			}
+
+			content = d
+			logger.Infof("Custom Kibana configuration detected at %s - this may affect Kibana behavior", customConfigPath)
+		})
+
+		if contentErr != nil {
+			return contentErr
+		}
+		if len(content) == 0 {
 			return nil
 		}
-		if err != nil {
-			return fmt.Errorf("failed to read custom kibana config: %w", err)
-		}
 
-		logger.Infof("Custom Kibana configuration detected at %s - this may affect Kibana behavior", customConfigPath)
-
-		if _, err = w.Write([]byte("\n\n# Custom Kibana Configuration\n")); err != nil {
+		if _, err := w.Write([]byte("\n\n# Custom Kibana Configuration\n")); err != nil {
 			return fmt.Errorf("failed to write custom config separator: %w", err)
 		}
 
-		if _, err = w.Write(customConfigData); err != nil {
+		if _, err := w.Write(content); err != nil {
 			return fmt.Errorf("failed to write custom kibana config: %w", err)
 		}
 
